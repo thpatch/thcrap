81a0b89d8bf02dd9bfa3254bfb28e1d4373fc0ac
nmlgc <nmlgc@thpatch.net>
2013-08-20 03:47:31 +0200

    Refactor the text layout parser.
    The string is now tokenized into an array before any actual parsing.

--- thcrap_tsa/src/layout.c
+++ thcrap_tsa/src/layout.c
@@ -34,40 +34,45 @@ int layout_match_set(json_t *arr, size_t ind, const char *str, size_t len)
 	}
 }
 
-// Matches at most [len] bytes of layout markup at the beginning of [str],
-// storing parameters in [arr].
-// Returns the full length of the layout markup in the original string.
-size_t layout_match(json_t *arr, const char *str, size_t len)
+// Matches at most [len] bytes of layout markup at the beginning of [str].
+// Returns an array with the layout parameters and, optionally,
+// the full length of the layout markup in [str] in [match_len].
+json_t* layout_match(size_t *match_len, const char *str, size_t len)
 {
 	const char *end = NULL;
 	const char *p = NULL;
 	const char *s = NULL; // argument start
+	json_t *ret = NULL;
 	size_t i = 0;
 	size_t ind = 0;
 
-	if(!json_is_array(arr) || !str || !len) {
-		return 0;
+	if(!str || !len) {
+		return ret;
 	}
 	if(str[0] != '<') {
-		return 0;
+		return ret;
 	}
 
 	end = memchr(str, '>', len);
 	if(!end) {
 		return 0;
 	}
+	ret = json_array();
 	len = end - str;
 	s = str + 1;
 	for(i = 1, p = s; i < len; i++, p++) {
 		if(str[i] == '$') {
-			layout_match_set(arr, ind, s, p - s);
+			layout_match_set(ret, ind, s, p - s);
 			s = p + 1;
 			ind++;
 		}
 	}
 	// Append final one
-	layout_match_set(arr, ind, s, p - s);
-	return len + 1;
+	layout_match_set(ret, ind, s, p - s);
+	if(match_len) {
+		*match_len = len + 1;
+	}
+	return ret;
 }
 
 BOOL WINAPI layout_TextOutU(
@@ -79,16 +84,19 @@ BOOL WINAPI layout_TextOutU(
 ) {
 	HBITMAP hBitmap = (HBITMAP)GetCurrentObject(hdc, OBJ_BITMAP);
 	HFONT hFontOrig = (HFONT)GetCurrentObject(hdc, OBJ_FONT);
-	HFONT hFontNew = NULL;
 
 	BOOL ret;
-	json_t *match = json_array();
+	json_t *tokens;
+	json_t *token;
 	LONG bitmap_width;
 
 	int i = 0;
 	int cur_x = orig_x;
 	size_t cur_tab = 0;
-	int font_recreate = 0;
+
+	if(!lpString || !c) {
+		return 0;
+	}
 
 	if(c >= strlen(lpString)) {
 		lpString = strings_lookup(lpString, &c);
@@ -103,31 +111,58 @@ BOOL WINAPI layout_TextOutU(
 		bitmap_width = dibsect.dsBm.bmWidth;
 	}
 
+	tokens = json_array();
+
+	// Split the string into an array of tokens to render in a sequence.
+	// These are either strings (= direct text) or arrays in itself (= layout commands).
 	while(i < c) {
-		LPCSTR draw_str = lpString + i;
-		int draw_str_len = c - i;
-		int advance_len = draw_str_len;
-		int match_len;
+		LPCSTR cur_str = lpString + i;
+		size_t cur_len = c - i;
+
+		json_t *match = layout_match(&cur_len, cur_str, cur_len);
+
+		// Requiring at least 2 parameters for a layout command still lets people
+		// write "<text>" without that being swallowed by the layout parser.
+		if(json_array_size(match) > 1) {
+			json_array_append_new(tokens, match);
+		} else {
+			char *cmd_start = (char*)memchr(cur_str + 1, '<', cur_len - 1);
+			if(cmd_start) {
+				cur_len = cmd_start - cur_str;
+			}
+			{
+				char *cur_str_token = EnsureUTF8(cur_str, cur_len);
+				json_array_append_new(tokens, json_string(cur_str_token));
+				SAFE_FREE(cur_str_token);
+			}
+		}
+		i += cur_len;
+	}
+
+	json_array_foreach(tokens, i, token) {
+		const char *draw_str = NULL;
+		HFONT hFontNew = NULL;
+		int font_recreate = 0;
 		size_t cur_w = 0;
 		SIZE str_size;
 
-		json_array_clear(match);
-		match_len = layout_match(match, draw_str, draw_str_len);
-		// Only do layout processing when everything up to the token has been printed
-		if(match_len) {
-			const char *cmd = json_array_get_string(match, 0);
-			const char *p1 = json_array_get_string(match, 1);
-			const char *p2 = json_array_get_string(match, 2);
+		if(json_is_array(token)) {
+			const char *cmd = json_array_get_string(token, 0);
+			const char *p1 = json_array_get_string(token, 1);
+			const char *p2 = json_array_get_string(token, 2);
+			const char *p = cmd;
 			// Absolute x-end position of the current tab
 			size_t tab_end;
+			// We're guaranteed to have at least p1 if we come here
+			size_t p1_len = strlen(p1) + 1;
+			LOGFONT font;
 
-			if(p1) {
-				advance_len = match_len;
-				draw_str_len = strlen(p1) + 1;
+			// could change after a layout command
+			GetObject(hFontOrig, sizeof(LOGFONT), &font);
 
-				GetTextExtentPoint32(hdc, p1, draw_str_len, &str_size);
-				cur_w = str_size.cx;
-			}
+			GetTextExtentPoint32(hdc, p1, p1_len, &str_size);
+			cur_w = str_size.cx;
+			draw_str = p1;
 
 			if(p2) {
 				// Use full bitmap with empty second parameter
@@ -146,89 +181,74 @@ BOOL WINAPI layout_TextOutU(
 				tab_end = cur_x + cur_w;
 			}
 
-			if(p1 && cmd) {
-				const char *p = cmd;
-				LOGFONT font;
-
-				GetObject(hFontOrig, sizeof(LOGFONT), &font);
-				while(*p) {
-					switch(p[0]) {
-						case 's':
-							// Don't actually print anything
-							tab_end = cur_w = 0;
-							p1 = NULL;
-							break;
-						case 't':
-							// Tabstop definition
-							{
-								// The width of the first parameter is already in cur_w, so...
-								size_t j = 2;
-								const char *str = NULL;
-								for(; j < json_array_size(match) && (str = json_array_get_string(match, j)); j++) {
-									GetTextExtentPoint32(hdc, str, strlen(str), &str_size);
-									cur_w = max(str_size.cx, cur_w);
-								}
+			while(*p) {
+				switch(p[0]) {
+					case 's':
+						// Don't actually print anything
+						tab_end = cur_w = 0;
+						draw_str = NULL;
+						break;
+					case 't':
+						// Tabstop definition
+						{
+							// The width of the first parameter is already in cur_w, so...
+							size_t j = 2;
+							const char *str = NULL;
+							for(; j < json_array_size(token) && (str = json_array_get_string(token, j)); j++) {
+								GetTextExtentPoint32(hdc, str, strlen(str), &str_size);
+								cur_w = max(str_size.cx, cur_w);
 							}
-							tab_end = cur_x + cur_w;
-							json_array_set_expand(Layout_Tabs, cur_tab, json_integer(tab_end - orig_x));
-							break;
-						case 'c':
-							// Center alignment
-							cur_x += ((tab_end - cur_x) / 2) - (cur_w / 2);
-							break;
-						case 'r':
-							// Right alignment
-							cur_x = (tab_end - cur_w);
-							break;
-						case 'b':
-							// Bold font
-							font.lfWeight *= 2;
-							font_recreate = 1;
-							break;
-						case 'i':
-							// Italic font
-							font.lfItalic = TRUE;
-							font_recreate = 1;
-							break;
-					}
-					p++;
+						}
+						tab_end = cur_x + cur_w;
+						json_array_set_expand(Layout_Tabs, cur_tab, json_integer(tab_end - orig_x));
+						break;
+					case 'c':
+						// Center alignment
+						cur_x += ((tab_end - cur_x) / 2) - (cur_w / 2);
+						break;
+					case 'r':
+						// Right alignment
+						cur_x = (tab_end - cur_w);
+						break;
+					case 'b':
+						// Bold font
+						font.lfWeight *= 2;
+						font_recreate = 1;
+						break;
+					case 'i':
+						// Italic font
+						font.lfItalic = TRUE;
+						font_recreate = 1;
+						break;
 				}
-				if(font_recreate) {
-					hFontNew = CreateFontIndirect(&font);
-					SelectObject(hdc, hFontNew);
-					GetTextExtentPoint32(hdc, p1, draw_str_len, &str_size);
-					tab_end = cur_x + str_size.cx;
-				}
-				if(tab_end) {
-					cur_tab++;
-					cur_w = tab_end - cur_x;
-				}
-				draw_str = p1;
+				p++;
 			}
-		}
-		// This matches both the standard case without any alignment, as well as
-		// the case where the layout markup doesn't contain any parameters.
-		// The last one is especially important, as it still lets people write
-		// "<text>" without that being swallowed by the layout parser.
-		if(!match_len || draw_str == lpString + i) {
-			char *cmd_start = (char*)memchr(draw_str + 1, '<', advance_len - 1);
-			if(cmd_start) {
-				draw_str_len = advance_len = cmd_start - draw_str;
+			if(font_recreate) {
+				hFontNew = CreateFontIndirect(&font);
+				SelectObject(hdc, hFontNew);
+				GetTextExtentPoint32(hdc, p1, p1_len, &str_size);
+				tab_end = cur_x + str_size.cx;
 			}
-			GetTextExtentPoint32(hdc, draw_str, draw_str_len, &str_size);
+			if(tab_end) {
+				cur_tab++;
+				cur_w = tab_end - cur_x;
+			}
+		} else if(json_is_string(token)) {
+			draw_str = json_string_value(token);
+			GetTextExtentPoint32(hdc, draw_str, strlen(draw_str), &str_size);
 			cur_w = str_size.cx;
 		}
-		ret = TextOutU(hdc, cur_x, orig_y, draw_str, draw_str_len);
-
+		if(draw_str) {
+			ret = TextOutU(hdc, cur_x, orig_y, draw_str, strlen(draw_str));
+		}
 		if(font_recreate) {
 			SelectObject(hdc, hFontOrig);
 			DeleteObject(hFontNew);
 			hFontNew = NULL;
 		}
 		cur_x += cur_w;
-		i += advance_len;
 	}
-	json_decref(match);
+	json_decref(tokens);
 	return ret;
 }
 
