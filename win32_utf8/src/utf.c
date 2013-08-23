be31e8df1ffaea5814baba5a1435901a059c48e8
nmlgc <nmlgc@thpatch.net>
2013-08-23 17:21:11 +0200

    Allow length calculations in the UTF conversion functions.
    You can now:
    * pass NULL as the destination parameter to get the required buffer size for the converted string
    * pass -1 as the string length to have it calculated automatically

--- win32_utf8/src/utf.c
+++ win32_utf8/src/utf.c
@@ -8,29 +8,33 @@
 
 #include "win32_utf8.h"
 
-int StringToUTF16(wchar_t *str_w, const char *str_mb, size_t str_len)
+int StringToUTF16(wchar_t *str_w, const char *str_mb, int str_len)
 {
-	int ret;
 	extern UINT fallback_codepage;
+	int ret;
+	int str_len_w;
 
 	if(!str_mb || !str_len) {
 		return 0;
 	}
-
-	ret = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str_mb, str_len, str_w, str_len);
+	if(str_len == -1) {
+		str_len = strlen(str_mb) + 1;
+	}
+	str_len_w = str_w ? str_len : 0;
+	ret = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str_mb, str_len, str_w, str_len_w);
 	if(!ret) {
 		if(str_mb[str_len - 1] != 0) {
 			// The previous conversion attempt still lingers in [str_w].
-			// If we don't clear it in case the original string isn't null-terminated,
-			// garbage may show up at the end of the converted string...
+			// If we don't clear it, garbage may show up at the end of the
+			// converted string if the original string wasn't null-terminated...
 			ZeroMemory(str_w, str_len * sizeof(wchar_t));
 		}
-		ret = MultiByteToWideChar(fallback_codepage, MB_PRECOMPOSED, str_mb, str_len, str_w, str_len);
+		ret = MultiByteToWideChar(fallback_codepage, MB_PRECOMPOSED, str_mb, str_len, str_w, str_len_w);
 	}
 	return ret;
 }
 
-wchar_t* StringToUTF16_VLA(wchar_t *str_w, const char *str_mb, size_t str_len)
+wchar_t* StringToUTF16_VLA(wchar_t *str_w, const char *str_mb, int str_len)
 {
 	if(str_mb) {
 		StringToUTF16(str_w, str_mb, str_len);
@@ -40,13 +44,13 @@ wchar_t* StringToUTF16_VLA(wchar_t *str_w, const char *str_mb, size_t str_len)
 	return str_w;
 }
 
-int StringToUTF8(char *str_utf8, const wchar_t *str_w, size_t str_len)
+int StringToUTF8(char *str_utf8, const wchar_t *str_w, int str_len)
 {
 	size_t str_utf8_len = str_len * sizeof(char) * UTF8_MUL;
 	return WideCharToMultiByte(CP_UTF8, 0, str_w, -1, str_utf8, str_utf8_len, NULL, NULL);
 }
 
-char* EnsureUTF8(const char *str, size_t str_len)
+char* EnsureUTF8(const char *str, int str_len)
 {
 	if(!str) {
 		return NULL;
