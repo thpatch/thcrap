bf745861f1347e4ddd734191bbb13369388a8aa4
nmlgc <nmlgc@thpatch.net>
2013-09-06 15:11:37 +0200

    Correctly determine the size of patched Win32 dialog items, once and for all.
    In the end, the correct way ended up involving a healthy dose of RTFM and the use of an undocumented Windows function (which we copy-paste from Wine, just to be safe). Hooray.

--- thcrap/src/dialog.c
+++ thcrap/src/dialog.c
@@ -154,6 +154,8 @@ typedef struct {
 	DLGTEMPLATEEX_START *dst_header;
 	HDC hDC;
 	HFONT hFont;
+	LONG base_w;
+	LONG base_h;
 } dialog_adjust_t;
 /// ----------
 
@@ -184,32 +186,86 @@ size_t memcpy_advance_src(unsigned char *dst, const unsigned char **src, size_t 
 
 /// dialog_adjust_t
 /// ---------------
+
+/**
+  * Wine source code below (dlls/gdi32/font.c).
+  * This is an internal Windows function - it's present and even exported since
+  * at least Windows XP (our minimum requirement anyway), but it's not supposed
+  * to be public by any means, so we'll rather include a copy of it here.
+  */
+
+/***********************************************************************
+ *           GdiGetCharDimensions    (GDI32.@)
+ *
+ * Gets the average width of the characters in the English alphabet.
+ *
+ * PARAMS
+ *  hdc    [I] Handle to the device context to measure on.
+ *  lptm   [O] Pointer to memory to store the text metrics into.
+ *  height [O] On exit, the maximum height of characters in the English alphabet.
+ *
+ * RETURNS
+ *  The average width of characters in the English alphabet.
+ *
+ * NOTES
+ *  This function is used by the dialog manager to get the size of a dialog
+ *  unit. It should also be used by other pieces of code that need to know
+ *  the size of a dialog unit in logical units without having access to the
+ *  window handle of the dialog.
+ *  Windows caches the font metrics from this function, but we don't and
+ *  there doesn't appear to be an immediate advantage to do so.
+ *
+ * SEE ALSO
+ *  GetTextExtentPointW, GetTextMetricsW, MapDialogRect.
+ */
+
+LONG WINAPI GdiGetCharDimensions(HDC hdc, LPTEXTMETRICW lptm, LONG *height)
+{
+    SIZE sz;
+    static const WCHAR alphabet[] = {
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+        'r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H',
+        'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',0};
+
+    if(lptm && !GetTextMetricsW(hdc, lptm)) return 0;
+
+    if(!GetTextExtentPointW(hdc, alphabet, 52, &sz)) return 0;
+
+    if (height) *height = sz.cy;
+    return (sz.cx / 26 + 1) / 2;
+}
+
 void dialog_adjust_init(
 	dialog_adjust_t *adj,
 	DLGTEMPLATEEX_START *dst_header,
-	const DLGTEMPLATEEX_FONT *font,
-	const sz_Or_Ord *typeface
+	const DLGTEMPLATEEX_FONT *dst_font
 )
 {
 	int font_height;
-	if(!adj || !dst_header || !font || !typeface) {
+	const sz_Or_Ord *typeface = NULL;
+	if(!adj || !dst_header || !dst_font) {
 		return;
 	}
 	ZeroMemory(adj, sizeof(*adj));
+	typeface = (sz_Or_Ord*)((BYTE*)dst_font + sizeof(DLGTEMPLATEEX_FONT));
+
 	adj->dst_header = dst_header;
 	adj->hDC = GetDC(0);
-
-	// I don't know why every other piece of code I've seen using this algorithm,
-	// including MSDN and Wine, puts a minus sign in front of the MulDiv call,
-	// but a positive return value is a lot closer to what we want here.
-	font_height = MulDiv(font->pointsize, GetDeviceCaps(adj->hDC, LOGPIXELSY), 72);
+	font_height = -MulDiv(dst_font->pointsize, GetDeviceCaps(adj->hDC, LOGPIXELSY), 72);
 
 	adj->hFont = CreateFontW(
-		font_height, 0, 0, 0, font->weight, font->italic, FALSE, FALSE,
+		font_height, 0, 0, 0, dst_font->weight, dst_font->italic, FALSE, FALSE,
 		DEFAULT_CHARSET, 0, 0, PROOF_QUALITY, FF_DONTCARE, &typeface->sz
 	);
+	/**
+	  * We need to do MapDialogRect without a dialog window, so we need to derive
+	  * the width and height of a dialog unit from the font.
+	  * GetTextMetrics() does *not* give the right values, this seems to be the
+	  * only function to work under all circumstances.
+	  */
 	if(adj->hFont) {
 		SelectObject(adj->hDC, adj->hFont);
+		adj->base_w = GdiGetCharDimensions(adj->hDC, NULL, &adj->base_h);
 	}
 }
 
@@ -228,18 +284,24 @@ void dialog_adjust(
 	rect.right = item->cx;
 	draw_flags = DT_CALCRECT | (strchr(str_new, '\n') ? DT_WORDBREAK : 0);
 	DrawText(adj->hDC, str_new, -1, &rect, draw_flags);
+
+	// Convert pixels back to dialog units
+	// (see http://msdn.microsoft.com/library/windows/desktop/ms645475%28v=vs.85%29.aspx)
+	rect.right = MulDiv(rect.right, 4, adj->base_w);
+	rect.bottom = MulDiv(rect.bottom, 8, adj->base_h);
+
 	button_style = item->style & 0xff;
 	/**
 	  * Why, Microsoft. Why.
 	  * There seems to be *no way* to determine this programmatically.
 	  * ... oh well, there are tons of dialog templates out there that implicitly
-	  * depend on that value being roughly 8, so...
+	  * depend on that graphic being 13 + base_w pixels wide, so...
 	  */
 	if(
 		(button_style == BS_CHECKBOX || button_style == BS_AUTOCHECKBOX ||
 		 button_style == BS_RADIOBUTTON || button_style == BS_AUTORADIOBUTTON)
 	) {
-		rect.right += 8;
+		rect.right += MulDiv(13 + adj->base_w, 4, adj->base_w);
 	} else if(button_style == BS_PUSHBUTTON || button_style == BS_DEFPUSHBUTTON) {
 		rect.right += 3;
 	}
@@ -347,7 +409,7 @@ size_t dialog_template_ex_build(BYTE *dst, const BYTE **src, dialog_adjust_t *ad
 		dst += sz_or_ord_build(dst, src, trans_title);
 
 		if(dst_header->style & DS_SETFONT) {
-			DLGTEMPLATEEX_FONT *font = (DLGTEMPLATEEX_FONT*)*src;
+			DLGTEMPLATEEX_FONT *dst_font = (DLGTEMPLATEEX_FONT*)dst;
 			size_t len;
 
 			len = memcpy_advance_src(dst, src, sizeof(DLGTEMPLATEEX_FONT));
@@ -356,9 +418,8 @@ size_t dialog_template_ex_build(BYTE *dst, const BYTE **src, dialog_adjust_t *ad
 			}
 			dst += len;
 
-			len = sz_or_ord_build(dst, src, trans_font); // typeface
-			dialog_adjust_init(adj, dst_header, font, (sz_Or_Ord*)dst);
-			dst += len;
+			dst += sz_or_ord_build(dst, src, trans_font); // typeface
+			dialog_adjust_init(adj, dst_header, dst_font);
 		}
 		*src = ptr_dword_align(*src);
 		return ptr_dword_align(dst) - dst_start;
