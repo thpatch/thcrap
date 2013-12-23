0d7d200f689d004afb2efe37eef9d6dd8ca762f2
nmlgc <nmlgc@thpatch.net>
2013-12-23 20:29:41 +0100

    thcrap_configure: Include the repository ID in the patch selection object.
    Also introducing more variable naming consistency.

--- thcrap_configure/src/select.c
+++ thcrap_configure/src/select.c
@@ -10,22 +10,29 @@
 
 #define PATCH_ID_LEN 16
 
-// Returns 1 if [patch_id] is [selected].
-int PatchIsSelected(json_t *selected, json_t *patch_id)
+// Returns 1 if the patch [patch_id] from [repo_id] is in [sel_stack].
+// [repo_id] can be NULL to ignore the repository.
+int IsSelected(json_t *sel_stack, json_t *repo_id, json_t *patch_id)
 {
-	json_t *sel_val;
+	json_t *sel;
 	size_t i;
 
-	json_array_foreach(selected, i, sel_val) {
-		if(json_equal(patch_id, sel_val)) {
+	json_array_foreach(sel_stack, i, sel) {
+		json_t *sel_repo = json_array_get(sel, 0);
+		json_t *sel_patch = json_array_get(sel, 1);
+		if(
+			(json_is_string(repo_id) ? json_equal(sel_repo, repo_id) : 1)
+			&& json_equal(sel_patch, patch_id)
+		) {
 			return 1;
 		}
 	}
 	return 0;
 }
 
-json_t* SelectPatchStack(json_t *repo_js, json_t *selected)
+json_t* SelectPatchStack(json_t *repo_js, json_t *sel_stack)
 {
+	json_t *repo_id = json_object_get(repo_js, "id");
 	json_t *list_order = json_array();
 	// Screen clearing offset line
 	SHORT y;
@@ -37,8 +44,8 @@ json_t* SelectPatchStack(json_t *repo_js, json_t *selected)
 		log_printf("\nNo patches available -.-\n");
 		goto end;
 	}
-	if(!json_is_array(selected)) {
-		selected = json_array();
+	if(!json_is_array(sel_stack)) {
+		sel_stack = json_array();
 	}
 
 	cls(0);
@@ -75,30 +82,31 @@ json_t* SelectPatchStack(json_t *repo_js, json_t *selected)
 		size_t list_count = 0;
 		char buf[16];
 		size_t list_pick;
-		json_t *patch;
+		json_t *sel;
+		const char *sel_patch;
 
 		json_array_clear(list_order);
 
 		cls(y);
 
-		if(json_array_size(selected)) {
+		if(json_array_size(sel_stack)) {
 			size_t i;
-			json_t *json_val;
+			json_t *sel;
 
 			printf("Selected patches (in ascending order of priority):\n\n");
 
-			json_array_foreach(selected, i, json_val) {
-				const char *patch_id = json_string_value(json_val);
+			json_array_foreach(sel_stack, i, sel) {
+				const char *patch_id = json_array_get_string(sel, 1);
 				const char *patch_title = json_object_get_string(patches, patch_id);
 
 				printf("  %2d. %-*s%s\n", ++list_count, PATCH_ID_LEN, patch_id, patch_title);
 
-				json_array_append(list_order, json_val);
+				json_array_append(list_order, sel);
 			}
 			printf("\n\n");
 		}
 
-		if(json_array_size(selected) < json_object_size(patches)) {
+		if(json_array_size(sel_stack) < json_object_size(patches)) {
 			json_t *json_val;
 			size_t i;
 			printf("Available patches:\n\n");
@@ -107,10 +115,11 @@ json_t* SelectPatchStack(json_t *repo_js, json_t *selected)
 				const char *patch_id = json_string_value(json_val);
 				const char *patch_title = json_object_get_string(patches, patch_id);
 
-				if(!PatchIsSelected(selected, json_val)) {
+				if(!IsSelected(sel_stack, repo_id, json_val)) {
+					json_t *sel = json_pack("[ss]", json_string_value(repo_id), patch_id);
 					printf(" [%2d] %-*s%s\n", ++list_count, PATCH_ID_LEN, patch_id, patch_title);
 
-					json_array_append(list_order, json_val);
+					json_array_append_new(list_order, sel);
 				}
 			}
 			printf("\n\n");
@@ -129,24 +138,25 @@ json_t* SelectPatchStack(json_t *repo_js, json_t *selected)
 		) {
 			break;
 		}
-		patch = json_array_get(list_order, list_pick - 1);
+		sel = json_array_get(list_order, list_pick - 1);
+		sel_patch = json_array_get_string(sel, 1);
 
-		if(!strcmp(json_string_value(patch), "base_tsa")) {
+		if(sel_patch && !strcmp(sel_patch, "base_tsa")) {
 			printf(
 				"\nUm... you _really_ do not want to mess with base_tsa.\n"
 				"This patch supplies the foundation for every other patch offered here.\n"
 				"If you remove it, none of those will work.\n\n");
 			pause();
 		} else {
-			if(list_pick > json_array_size(selected)) {
-				json_array_append(selected, patch);
+			if(list_pick > json_array_size(sel_stack)) {
+				json_array_append(sel_stack, sel);
 			} else {
-				json_array_remove(selected, list_pick - 1);
+				json_array_remove(sel_stack, list_pick - 1);
 			}
 		}
 	}
 end:
 	json_decref(list_order);
 	json_decref(patches_sorted);
-	return selected;
+	return sel_stack;
 }
