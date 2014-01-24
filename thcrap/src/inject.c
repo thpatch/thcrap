thpatch/thcrap

commit f4b1db75e905a443b814b5dddd1e28f49233f6c9

Use LoadLibraryEx() in the injection payload to guarantee the correct DLL path.
No more issues with older thcrap proxy DLLs... or rather, users extracting thcrap to a game directory and then deciding against it later.

thcrap/src/inject.c
@@ -91,10 +91,15 @@ int Inject(HANDLE hProcess, const char *dll_dir, const char *dll_fn, const char 
 	// Main DLL we will need to load
 	HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
 
-	// Main functions we will need to import
+	// Main functions we will need to import.
+	// If [dll_fn] is absolute, LoadLibraryEx() with the LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR
+	// flag is used to guarantee that the injected DLL and its dependencies really
+	// are only loaded from the given directory. Otherwise, LoadLibrary() may load
+	// a possible other DLL with the same name from the directory of [hProcess].
 	FARPROC getcurrentdirectory = GetProcAddress(kernel32, "GetCurrentDirectoryW");
 	FARPROC setcurrentdirectory = GetProcAddress(kernel32, "SetCurrentDirectoryW");
 	FARPROC loadlibrary = GetProcAddress(kernel32, "LoadLibraryW");
+	FARPROC loadlibraryex = GetProcAddress(kernel32, "LoadLibraryExW");
 	FARPROC getprocaddress = GetProcAddress(kernel32, "GetProcAddress");
 	FARPROC exitthread = GetProcAddress(kernel32, "ExitThread");
 	FARPROC freelibraryandexitthread = GetProcAddress(kernel32, "FreeLibraryAndExitThread");
@@ -291,7 +296,7 @@ int Inject(HANDLE hProcess, const char *dll_dir, const char *dll_fn, const char 
 	}
 
 	// Load the injected DLL into this process
-	HMODULE h = LoadLibrary(dll_fn);
+	HMODULE h = LoadLibraryEx(dll_fn, NULL, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
 	if(!h) {
 		MessageBox(0, "Could not load the dll: mydll.dll", "Error", MB_ICONERROR);
 		ExitThread(1);
@@ -397,15 +402,32 @@ int Inject(HANDLE hProcess, const char *dll_dir, const char *dll_fn, const char 
 		*p++ = 0xD6;
 	}
 
-	// PUSH 0x00000000 - Push the address of the DLL name to use in LoadLibrary
+	if(PathIsRelativeA(dll_fn)) {
+		// PUSH 0x00 (dwFlags = 0)
+		*p++ = 0x6a;
+		*p++ = 0x00;
+	} else {
+		// PUSH 0x00000100 (dwFlags = LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR)
+		*p++ = 0x68;
+		*p++ = 0x00;
+		*p++ = 0x01;
+		*p++ = 0x00;
+		*p++ = 0x00;
+	}
+
+	// PUSH 0x00 (hFile = NULL)
+	*p++ = 0x6a;
+	*p++ = 0x00;
+
+	// PUSH 0x00000000 - Push the address of the DLL name to use in LoadLibraryEx
 	*p++ = 0x68;
 	p = ptrcpy_advance_dst(p, dllNameAddr);
 
-	// MOV EAX, ADDRESS - Move the address of LoadLibrary into EAX
+	// MOV EAX, ADDRESS - Move the address of LoadLibraryEx into EAX
 	*p++ = 0xB8;
-	p = ptrcpy_advance_dst(p, loadlibrary);
+	p = ptrcpy_advance_dst(p, loadlibraryex);
 
-	// CALL EAX - Call LoadLibrary
+	// CALL EAX - Call LoadLibraryEx
 	*p++ = 0xFF;
 	*p++ = 0xD0;
 
@@ -659,7 +681,7 @@ int thcrap_inject(HANDLE hProcess, const char *run_cfg_fn)
 		const char *param;
 
 		GetModuleFileNameU(inj_mod, inj_dir, inj_dir_len);
-		strncpy(inj_dll, PathFindFileNameA(inj_dir), inj_dir_len);
+		strncpy(inj_dll, inj_dir, inj_dir_len);
 		PathRemoveFileSpec(inj_dir);
 		PathAddBackslashA(inj_dir);
 

commit 30c4bca0eb03bb3cba12a29ac82e3db7eee49e20

Tell the user to download KB2533623 if DLL injection failed.
Another cause for DLL injection failure, and hopefully the last one. The LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR flag of LoadLibraryEx is unsupported on Windows XP, but the function works just fine otherwise. However, on Windows Vista and 7 installations *without* KB2533623 installed, the function just outright fails entirely if that flag is given. As a result, f4b1db75e905a443b814b5dddd1e28f49233f6c9 broke thcrap for pretty much everyone who hasn't updated their Windows Vista or 7 since KB2533623 was published in 2011.

Working around this issue is both painful and counter-productive (keep your systems updated, will you!). Thus, we just tell people to get that specific system update instead.

Of course we could have only mentioned that update if the user actually *is* running Vista or 7. However, with Windows 8.1, GetVersionInfoEx() was deprecated and replaced by the new Version Helper APIs (http://msdn.microsoft.com/en-us/library/windows/desktop/dn424972%28v=vs.85%29.aspx). These are, of course, exclusively available in the Windows 8.1 SDK, which I have been failing to integrate into my build environment.

thcrap/src/inject.c
@@ -81,7 +81,12 @@ unsigned char* StringToUTF16_advance_dst(unsigned char *dst, const char *src)
 int Inject(HANDLE hProcess, const char *dll_dir, const char *dll_fn, const char *func_name, const void *param, const size_t param_size)
 {
 	// String constants
-	const char *injectError1Format = "Could not load the dll: %s";
+	const char *injectError1Format =
+		"Could not inject %s.\n"
+		"\n"
+		"If you're running Windows Vista or 7, make sure that you have installed the KB2533623 update:\n"
+		"\n"
+		"\thttp://support.microsoft.com/kb/2533623/";
 	const char *injectError2Format = "Could not load the function: %s";
 
 //------------------------------------------//