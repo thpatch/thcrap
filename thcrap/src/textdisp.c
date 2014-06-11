23258328e5707609cb2fc781df7785273701b96a
nmlgc <nmlgc@nmlgc.net>
2014-06-11 18:11:05 +0200

    textdisp: Add a rule-based font replacement system.
    Implements #19.

--- thcrap/src/textdisp.c
+++ thcrap/src/textdisp.c
@@ -36,6 +36,149 @@ const char* logfont_stringify(const LOGFONTA *lf)
 }
 /// -----------------------
 
+/// Font rules
+/// ----------
+// Fills [arg] with the font rule argument at [str].
+// Returns [str], advanced to the next argument.
+const char* fontrule_arg(char *arg, size_t arg_len, const char *str)
+{
+	size_t i = 0;
+	char delim = ' ';
+	if(!arg || !arg_len || !str) {
+		return str;
+	}
+	if(*str == '\'') {
+		delim = '\'';
+		str++;
+	}
+	while(i < (arg_len - 1) && *str && *str != delim) {
+		arg[i++] = *str++;
+	}
+	arg[i] = '\0';
+	// Skip any garbage after the argument...
+	while(*str && *str != ' ') {
+		str++;
+	}
+	// ...and skip from there to the next one.
+	while(*str && *str == ' ') {
+		str++;
+	}
+	return str;
+}
+
+int fontrule_arg_int(const char **str, int *score)
+{
+	int ret;
+	char arg[LF_FACESIZE] = {0};
+	if(!str || !*str) {
+		return 0;
+	}
+	*str = fontrule_arg(arg, sizeof(arg), *str);
+	ret = atoi(arg);
+	if(score) {
+		*score += ret != 0;
+	}
+	return ret;
+}
+
+// Returns the amount of LOGFONT parameters (the "score") contained in [str].
+int fontrule_parse(LOGFONTA *lf, const char *str)
+{
+	int score = 0;
+	if(!lf || !str) {
+		return -1;
+	}
+	if(*str == '\'') {
+		str = fontrule_arg(lf->lfFaceName, LF_FACESIZE, str);
+		score++;
+	} else {
+		lf->lfPitchAndFamily = fontrule_arg_int(&str, &score);
+		lf->lfFaceName[0] = '\0';
+	}
+	lf->lfHeight = fontrule_arg_int(&str, &score);
+	lf->lfWidth = fontrule_arg_int(&str, &score);
+	lf->lfWeight = fontrule_arg_int(&str, &score);
+	return score;
+}
+
+#define FONTRULE_MATCH(rule, dst, val) \
+	if(rule##->##val && rule##->##val != dst##->##val) {\
+		return 0; \
+	}
+
+#define FONTRULE_APPLY(dst, rep, val) \
+	if(rep##->##val && priority ? 1 : !dst##->##val) { \
+		dst##->##val = rep##->##val; \
+	}
+
+#define FONTRULE_MACRO_EXPAND(macro, p1, p2) \
+	macro(p1, p2, lfPitchAndFamily); \
+	macro(p1, p2, lfHeight); \
+	macro(p1, p2, lfWidth); \
+	macro(p1, p2, lfWeight);
+
+// Returns 1 if the relevant nonzero members in [rule] match those in [dst].
+int fontrule_match(const LOGFONTA *rule, const LOGFONTA *dst)
+{
+	if(rule->lfFaceName[0] && strncmp(
+		rule->lfFaceName, dst->lfFaceName, LF_FACESIZE
+	)) {
+		return 0;
+	}
+	FONTRULE_MACRO_EXPAND(FONTRULE_MATCH, rule, dst);
+	return 1;
+}
+
+// Copies all relevant nonzero members from [rep] to [dst]. If [priority] is
+// zero, values are only copied if the same value is not yet set in [dst].
+int fontrule_apply(LOGFONTA *dst, const LOGFONTA *rep, int priority)
+{
+	if(rep->lfFaceName[0] && priority ? 1 : !dst->lfFaceName[0]) {
+		strncpy(dst->lfFaceName, rep->lfFaceName, LF_FACESIZE);
+	}
+	FONTRULE_MACRO_EXPAND(FONTRULE_APPLY, dst, rep);
+	return 1;
+}
+
+// Returns 1 if a font rule was applied, 0 otherwise.
+int fontrules_apply(LOGFONTA *lf)
+{
+	json_t *fontrules = json_object_get(runconfig_get(), "fontrules");
+	LOGFONTA rep_full = {0};
+	int rep_score = 0;
+	int log_header = 0;
+	const char *key;
+	json_t *val;
+	if(!lf) {
+		return -1;
+	}
+	json_object_foreach(fontrules, key, val) {
+		LOGFONTA rule = {0};
+		int rule_score = fontrule_parse(&rule, key);
+		int priority = rule_score >= rep_score;
+		const char *rep_str = json_string_value(val);
+		if(!fontrule_match(&rule, lf)) {
+			continue;
+		}
+		if(!log_header) {
+			log_printf(
+				"(Font) Replacement rules applied to %s:\n",
+				logfont_stringify(lf)
+			);
+			log_header = 1;
+		}
+		fontrule_parse(&rule, rep_str);
+		log_printf(
+			"(Font) • (%s) → (%s)%s\n",
+			key, rep_str, priority ? " (priority)" : ""
+		);
+		fontrule_apply(&rep_full, &rule, priority);
+		rep_score = max(rule_score, rep_score);
+	}
+	return fontrule_apply(lf, &rep_full, 1);
+}
+/// ------------------
+
 // This detour is kept for backwards compatibility to patch configurations
 // that replace multiple fonts via hardcoded string translation. Due to the
 // fact that lower levels copy [pszFaceName] into the LOGFONT structure,
@@ -88,6 +231,7 @@ HFONT WINAPI textdisp_CreateFontIndirectExA(
 	// Ensure that the font face is in UTF-8
 	StringToUTF16(face_w, lf->lfFaceName, LF_FACESIZE);
 	StringToUTF8(lf->lfFaceName, face_w, LF_FACESIZE);
+	fontrules_apply(lf);
 	/**
 	  * CreateFont() prioritizes [lfCharSet] and ensures that the font
 	  * created can display the given charset. If the font given in
