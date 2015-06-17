8de7c2723ad2bd1bed3d7da0165bb243b289e30f
nmlgc <nmlgc@nmlgc.net>
2015-06-17 18:22:39 +0200

    Refactor printf format specifier parsing into its own function.
    This will be moved to win32_utf8 since we need it there for FormatMessage().

--- thcrap/src/strings.c
+++ thcrap/src/strings.c
@@ -8,6 +8,7 @@
   */
 
 #include "thcrap.h"
+#include <assert.h>
 
 /// Detour chains
 /// -------------
@@ -54,33 +55,105 @@ const char* strings_lookup(const char *in, size_t *out_len)
 	return ret;
 }
 
-va_list printf_parse_num(va_list va, const char **p)
+// Information about a single printf format specification.
+typedef struct {
+	int argc_before_type;
+	char type;
+	int type_size_in_ints;
+} printf_format_t;
+
+// Returns whether the numeric specifier at [p] consumes an argument or not,
+// and advances [p] past it.
+static int printf_num_parse(const char **p)
 {
+	assert(p && *p);
 	if(**p == '*') {
 		// width specified in next variable argument
 		(*p)++;
-		va_arg(va, int);
+		return 1;
 	} else while(isdigit(**p)) {
 		(*p)++;
 	}
-	return va;
+	return 0;
 }
 
-// The printf format parsing below is based on Wine's implementation for
-// msvcrt.dll (dlls/msvcrt/printf.h).
+// Fills [fmt] with information about the printf format specification
+// starting at [p]. [p] is assumed to start after the initial % character,
+// and is returned after the final character denoting the type.
+// Based on Wine's implementation for msvcrt.dll (dlls/msvcrt/printf.h).
+const char* printf_format_parse(printf_format_t *fmt, const char *p)
+{
+	int flag_double = 0;
+	
+	assert(fmt);
+	assert(p && *(p - 1) == '%');
+
+	memset(fmt, 0, sizeof(printf_format_t));
+
+	// Skip flags. From left to right:
+	// prefix sign, prefix space, left-align, zero padding, alternate
+	while(strchr("+ -0#", *p) != NULL) {
+		p++;
+	}
+
+	// Width
+	fmt->argc_before_type += printf_num_parse(&p);
+
+	// Precision
+	if(*p == '.') {
+		p++;
+		fmt->argc_before_type += printf_num_parse(&p);
+	}
+
+	// Argument size modifier
+	while(*p) {
+		if(*p=='l' && *(p+1)=='l') {
+			flag_double = 1;
+			p += 2;
+		} else if(*p=='h' || *p=='l' || *p=='L') {
+			p++;
+		} else if(*p == 'I') {
+			if(*(p+1)=='6' && *(p+2)=='4') {
+				flag_double = 1;
+				p += 3;
+			} else if(*(p+1)=='3' && *(p+2)=='2') {
+				p += 3;
+			} else if(isdigit(*(p+1)) || !*(p+1)) {
+				break;
+			} else {
+				p++;
+			}
+		} else if(*p == 'w' || *p == 'F') {
+			p++;
+		} else {
+			break;
+		}
+	}
+	fmt->type = *(p++);
+	if(strchr("aeEfgG", fmt->type)) {
+		// Two surprises here:
+		// • Yes, floats are always coerced to doubles.
+		// • Yes, flag_double is ignored here because Microsoft has no support
+		//   for printing long doubles in either of their printf implementations.
+		fmt->type_size_in_ints = sizeof(double) / sizeof(int);
+	} else if(strchr("sScCpndiouxX", fmt->type)) {
+		fmt->type_size_in_ints = flag_double + 1;
+	}
+	return p;
+}
+
 void strings_va_lookup(va_list va, const char *format)
 {
 	const char *p = format;
 	while(*p) {
-		char fmt;
-		int flag_double = 0;
+		printf_format_t fmt;
+		int i;
 
 		// Skip characters before '%'
 		for(; *p && *p != '%'; p++);
 		if(!*p) {
 			break;
 		}
-
 		// *p == '%' here
 		p++;
 
@@ -89,61 +162,16 @@ void strings_va_lookup(va_list va, const char *format)
 			p++;
 			continue;
 		}
-
-		// Skip flags. From left to right:
-		// prefix sign, prefix space, left-align, zero padding, alternate
-		while(strchr("+ -0#", *p) != NULL) {
-			p++;
+		p = printf_format_parse(&fmt, p);
+		for(i = 0; i < fmt.argc_before_type; i++) {
+			va_arg(va, int);
 		}
-
-		// Width
-		va = printf_parse_num(va, &p);
-
-		// Precision
-		if(*p == '.') {
-			p++;
-			va = printf_parse_num(va, &p);
-		}
-
-		// Argument size modifier
-		while(*p) {
-			if(*p=='l' && *(p+1)=='l') {
-				flag_double = 1;
-				p += 2;
-			} else if(*p=='h' || *p=='l' || *p=='L') {
-				p++;
-			} else if(*p == 'I') {
-				if(*(p+1)=='6' && *(p+2)=='4') {
-					flag_double = 1;
-					p += 3;
-				} else if(*(p+1)=='3' && *(p+2)=='2') {
-					p += 3;
-				} else if(isdigit(*(p+1)) || !*(p+1)) {
-					break;
-				} else {
-					p++;
-				}
-			} else if(*p == 'w' || *p == 'F') {
-				p++;
-			} else {
-				break;
-			}
-		}
-		fmt = *p;
-		if(fmt == 's' || fmt == 'S') {
+		if(fmt.type == 's' || fmt.type == 'S') {
 			*(const char**)va = strings_lookup(*(const char**)va, NULL);
 		}
-		// Advance [va] if the format is among the valid ones
-		if(strchr("aeEfgG", fmt)) {
-			va_arg(va, double);
-		}
-		if(strchr("sScCpndiouxX", fmt)) {
+		for(i = 0; i < fmt.type_size_in_ints; i++) {
 			va_arg(va, int);
-			if(flag_double) {
-				va_arg(va, int);
-			}
 		}
-		p++;
 	}
 }
 
