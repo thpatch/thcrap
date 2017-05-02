56f3e7fe0f02952ba5d57df0faf7fc5576e3b796
nmlgc <nmlgc@nmlgc.net>
2017-05-02 15:31:40 +0200

    TSA: MSG: Move the encraption functions into th06_msg.c.
    So, rather than depending on thcrap's specs module, and thcrap's
    jsondata module, and thcrap's jansson_ex.c, which by definition has to
    include GetModuleHandle(), and then setting up the build for both thcrap
    and the scripts… yeah, let's get better get rid of formats.js and
    hardcode these instead. Especially since it rarely changes these days,
    if at all. But if it does, it'll probably be a significant change that
    requires a code update anyway.
    
    And it'll be way easier to debug.
    
    (Really, formats.js stopped making sense as soon as we had automatic
    self-updates.)

--- thcrap_tsa/src/th06_msg.c
+++ thcrap_tsa/src/th06_msg.c
@@ -45,6 +45,36 @@ typedef struct {
 	const char *type;
 } op_info_t;
 
+/// Text encryption
+/// ---------------
+void simple_xor(
+	uint8_t *data, size_t data_len, const uint8_t *params, size_t params_count
+)
+{
+	size_t i;
+	if(!params || params_count < 1) {
+		return;
+	}
+	for(i = 0; i < data_len; i++) {
+		data[i] ^= params[0];
+	}
+}
+
+void util_xor(
+	uint8_t *data, size_t data_len, const uint8_t *params, size_t params_count
+)
+{
+	size_t i;
+	if(!params || params_count < 3) {
+		return;
+	}
+	for(i = 0; i < data_len; i++) {
+		const int ip = i - 1;
+		data[i] ^= params[0] + i * params[1] + (ip * ip + ip) / 2 * params[2];
+	}
+}
+/// --------------------
+
 typedef struct {
 	// Format information
 	op_info_t* op_info;
