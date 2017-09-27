7754bcd7cb32b9928dc1e03fd274995a26a8f1b4
brliron <brliron@hotmail.fr>
2017-09-27 15:49:52 +0200

    - Remove stack_update_with_UI_progress from thcrap_wrapper_update. - Add some compile-time checks to ensure I won't forget something like that in the future.

--- thcrap/src/thcrap_update_wrapper.h
+++ thcrap/src/thcrap_update_wrapper.h
@@ -15,18 +15,21 @@
 extern "C" {
 #endif
 
-void* ServerDownloadFile_wrapper(json_t *servers, const char *fn, DWORD *file_size, const DWORD *exp_crc, file_callback_t callback, void *callback_param);
+#define DECLARE_WRAPPER(ret, func, ...) typedef ret (*func##_type)(__VA_ARGS__); ret func##_wrapper(__VA_ARGS__);
 
-int update_filter_global_wrapper(const char *fn, json_t *null);
-int update_filter_games_wrapper(const char *fn, json_t *games);
-int patch_update_wrapper(json_t *patch_info, update_filter_func_t filter_func, json_t *filter_data, patch_update_callback_t callback, void *callback_param);
-void stack_update_wrapper(update_filter_func_t filter_func, json_t *filter_data, stack_update_callback_t callback, void *callback_param);
-void stack_update_with_UI_progress_wrapper(update_filter_func_t filter_func, json_t *filter_data);
+DECLARE_WRAPPER(void*, ServerDownloadFile, json_t *servers, const char *fn, DWORD *file_size, const DWORD *exp_crc, file_callback_t callback, void *callback_param);
 
-int RepoDiscoverAtURL_wrapper(const char *start_url, json_t *id_cache, json_t *url_cache);
-int RepoDiscoverFromLocal_wrapper(json_t *id_cache, json_t *url_cache);
+DECLARE_WRAPPER(int, update_filter_global, const char *fn, json_t *null);
+DECLARE_WRAPPER(int, update_filter_games, const char *fn, json_t *games);
+DECLARE_WRAPPER(int, patch_update, json_t *patch_info, update_filter_func_t filter_func, json_t *filter_data, patch_update_callback_t callback, void *callback_param);
+DECLARE_WRAPPER(void, stack_update, update_filter_func_t filter_func, json_t *filter_data, stack_update_callback_t callback, void *callback_param);
 
-json_t* patch_bootstrap_wrapper(const json_t *sel, json_t *repo_servers);
+DECLARE_WRAPPER(int, RepoDiscoverAtURL, const char *start_url, json_t *id_cache, json_t *url_cache);
+DECLARE_WRAPPER(int, RepoDiscoverFromLocal, json_t *id_cache, json_t *url_cache);
+
+DECLARE_WRAPPER(json_t*, patch_bootstrap, const json_t *sel, json_t *repo_servers);
+
+#undef DECLARE_WRAPPER
 
 #ifdef __cplusplus
 }
