8b76bf4e5db6ae115e50540657868dc0503afdf6
brliron <brliron@hotmail.fr>
2017-11-30 15:11:46 +0100

    thcrap_tasofro: use a theme_id <=> theme_line map for th123 [V]

--- thcrap_tasofro/src/bgm.cpp
+++ thcrap_tasofro/src/bgm.cpp
@@ -11,6 +11,7 @@
 #include "thcrap_tasofro.h"
 #include <vfs.h>
 #include "bgm.h"
+#include <algorithm>
 
 json_t* bgm_generator(std::unordered_map<std::string, json_t*> in_data, const std::string out_fn, size_t* out_size)
 {
@@ -84,5 +85,49 @@ json_t* bgm_generator(std::unordered_map<std::string, json_t*> in_data, const st
 	if (game_id >= TH135) {
 		*out_size += 512; // Because these files are zipped, guessing their exact output size is hard. We'll add a few more bytes.
 	}
+	return ret;
+}
+
+
+json_t* bgm_generate_from_map(std::unordered_map<std::string, json_t*> in_data, const std::string out_fn, size_t* out_size)
+{
+	if (out_size) {
+		*out_size = 0;
+	}
+
+	const char *spell_col = nullptr;
+
+	// Find themes.js and the map file in the input files list
+	auto themes_it = in_data.find("themes.js");
+	auto map_it = std::find_if(in_data.begin(), in_data.end(), [](std::pair<const std::string, json_t*>& value) -> bool {
+		int offset = value.first.length() - strlen(".map.jdiff");
+		if (offset < 0) {
+			return false;
+		}
+		return value.first.compare(offset, std::string::npos, ".map.jdiff") == 0;
+	});
+	if (themes_it == in_data.end() || map_it == in_data.end()) {
+		return NULL;
+	}
+	json_t *themes = themes_it->second;
+	json_t *themes_map = map_it->second;
+
+	json_t *ret = json_object();
+	size_t i;
+	json_t *value;
+	json_array_foreach(themes_map, i, value) {
+		const char* theme_id = json_object_get_string(value,  "id");
+		const char* line     = json_object_get_string(value,  "line");
+		const char* col      = json_object_get_string(value,  "col");
+		const char* theme    = json_object_get_string(themes, theme_id);
+		if (!theme || !line || !col) {
+			continue;
+		}
+
+		json_t *line_obj = json_object_get_create(ret, line, JSON_OBJECT);
+		json_object_set_new(line_obj, col, json_string(theme));
+		*out_size += strlen(theme);
+	}
+
 	return ret;
 }
