e32c9e41a2bd6c3c21f9d9a1c9a37cd11c210e39
nmlgc <nmlgc@nmlgc.net>
2018-01-02 04:59:12 +0100

    configure: Extend questions to support default answers.

--- thcrap_configure/src/configure.h
+++ thcrap_configure/src/configure.h
@@ -20,13 +20,27 @@ json_t* SelectPatchStack(json_t *repo_list);
 // Shows a file write error and asks the user if they want to continue
 int file_write_error(const char *fn);
 
+#define DEFAULT_ANSWER 0x80
+
 // Asks an optional [question] until one of the [answers] was entered, and
 // returns the given answer in lowercase. This check is case-insensitive.
+// Set the DEFAULT_ANSWER bit on one of the answers to automatically pick it
+// if nothing was entered.
 template <size_t N = 2> char Ask(
 	const char *question,
-	std::array<char, N> answers = { 'y', 'n' }
+	std::array<unsigned char, N> answers = { 'y', 'n' }
 )
 {
+	char defopt = '\0';
+	for(size_t i = 0; i < answers.size(); i++) {
+		auto c = answers[i];
+		if(c & DEFAULT_ANSWER) {
+			assert(defopt == '\0' || !"Only one option can be the default");
+			defopt = c & ~DEFAULT_ANSWER;
+		}
+		answers[i] = tolower(c & ~DEFAULT_ANSWER);
+	}
+
 	while(1) {
 		char buf[2];
 		if(question) {
@@ -35,18 +49,26 @@ template <size_t N = 2> char Ask(
 		}
 		log_print("(");
 		for(size_t i = 0; i < answers.size(); i++) {
-			auto c = tolower(answers[i]);
+			auto c = answers[i];
 			if(i >= 1) {
 				log_print("/");
 			}
-			log_printf("%c", c);
+			if(c == defopt) {
+				log_printf("[%c]", c);
+			} else {
+				log_printf("%c", c);
+			}
 		}
 		log_print(") ");
 
 		console_read(buf, sizeof(buf));
 		char ret = tolower(buf[0]);
+		if(ret == '\0') {
+			ret = defopt;
+		}
+
 		for(auto c : answers) {
-			if(ret == tolower(c)) {
+			if(ret == c) {
 				return ret;
 			}
 		}
