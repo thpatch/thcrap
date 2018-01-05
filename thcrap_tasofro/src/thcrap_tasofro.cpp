762fa76c1cac4b63a7e235f90dfe365cce242f8a
nmlgc <nmlgc@nmlgc.net>
2018-01-05 09:49:44 +0100

    thcrap_tasofro: Add the Steam AppID for th155. [V]
    Could have just been a char* array, but who knows if they do add trial
    versions later, so let's keep this structure.

--- thcrap_tasofro/src/thcrap_tasofro.cpp
+++ thcrap_tasofro/src/thcrap_tasofro.cpp
@@ -43,6 +43,16 @@ static tasofro_game_t game_id_from_string(const char *game)
 	return TH_FUTURE;
 }
 
+extern "C" __declspec(dllexport) const char* steam_appid(void)
+{
+	switch(game_id) {
+	case TH155:
+		return "716710";
+	default: // -Wswitch...
+		return nullptr;
+	}
+}
+
 int __stdcall thcrap_plugin_init()
 {
 	int base_tasofro_removed = stack_remove_if_unneeded("base_tasofro");
