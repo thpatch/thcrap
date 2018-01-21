0ed1df4d3e8d1f3940ededb32cb38dea5d322e26
nmlgc <nmlgc@nmlgc.net>
2018-01-21 23:26:29 +0100

    TSA: Support BGM loop patching for TH07 and later. [V]
    Funded by Splashman.
    
    How to use:
    • Make sure you got a sample-perfect extraction of the track whose
      loops you want to change. Touhou Music Room with
      	☐ Remove opening silence
      unchecked and FLAC format will work.
    • Check `musiccmt.txt` for the filename of the track.
    • Add a file named `thXX/thbgm.fmt.jdiff` (`albgm.fmt.jdiff` for
      Uwabami Breakers) to your patch with the following format:
    
    {
        "th11_15.wav": {
            "loop_start": 365318,
            "loop_end": 1436150
        }
    }
    
      With this being the positions for Last Remote in Limbo. All values
      are in samples.
    • This feature supports repatching! No need to restart the game process
      to apply changes to the loop sections. You only have to restart the
      specific track in the game's Music Room afterwards.

---
+++ thcrap_tsa/src/bgm.cpp
@@ -0,0 +1,110 @@
+/**
+  * Touhou Community Reliant Automatic Patcher
+  * Team Shanghai Alice support plugin
+  *
+  * ----
+  *
+  * BGM patching.
+  */
+
+#include <thcrap.h>
+extern "C" {
+# include <thtypes/bgm_types.h>
+# include "thcrap_tsa.h"
+}
+
+// For repatchability. Pointing into game memory.
+bgm_fmt_t* bgm_fmt = nullptr;
+
+bgm_fmt_t* bgm_find(stringref_t fn)
+{
+	assert(bgm_fmt);
+	if(fn.len > sizeof(bgm_fmt->fn)) {
+		return nullptr;
+	}
+	auto track = bgm_fmt;
+	while(track->fn[0] != '\0') {
+		if(!strncmp(track->fn, fn.str, sizeof(track->fn))) {
+			return track;
+		}
+		track++;
+	}
+	return nullptr;
+};
+
+int loopmod_fmt(json_t *fmt_jdiff, const char *jdiff_fn)
+{
+	int modded = 0;
+	auto mod = [jdiff_fn] (stringref_t fn_to_patch, const json_t* track_mod) {
+		auto track = bgm_find(fn_to_patch);
+		if(!track) {
+			log_mboxf(nullptr, MB_OK | MB_ICONEXCLAMATION,
+				"Error applying %s: Track \"%s\" does not exist.",
+				jdiff_fn, fn_to_patch.str
+			);
+			return false;
+		}
+
+		auto sample_size = track->wfx.nChannels * (track->wfx.wBitsPerSample / 8);
+		auto *loop_start = json_object_get(track_mod, "loop_start");
+		auto *loop_end = json_object_get(track_mod, "loop_end");
+
+		if(loop_start || loop_end) {
+			log_printf("[BGM] [Loopmod] Changing %s\n", fn_to_patch.str);
+		}
+		if(loop_start) {
+			uint32_t val = (uint32_t)json_integer_value(loop_start);
+			track->intro_size = val * sample_size;
+		}
+		if(loop_end) {
+			uint32_t val = (uint32_t)json_integer_value(loop_end);
+			track->track_size = val * sample_size;
+		}
+		return true;
+	};
+
+	// Looping this way makes error reporting a bit easier.
+	const char *key;
+	const json_t *track_mod;
+	json_object_foreach(fmt_jdiff, key, track_mod) {
+		modded += mod(key, track_mod);
+	}
+	return modded;
+}
+
+/// Hooks
+/// =====
+size_t keep_original_size(const char *fn, json_t *patch, size_t patch_size)
+{
+	return 0;
+}
+
+int patch_fmt(void *file_inout, size_t size_out, size_t size_in, const char *fn, json_t *patch)
+{
+	bgm_fmt = (bgm_fmt_t*)file_inout;
+	return loopmod_fmt(patch, fn);
+}
+
+extern "C" __declspec(dllexport) void bgm_mod_init(void)
+{
+	// Kioh Gyoku is a thing...
+	if(game_id < TH07) {
+	} else {
+		// albgm.fmt and trial versions are also a thing...
+		patchhook_register("*bgm*.fmt", patch_fmt, keep_original_size);
+	}
+}
+
+extern "C" __declspec(dllexport) void bgm_mod_repatch(json_t *files_changed)
+{
+	const char *fn;
+	json_t *val;
+	json_object_foreach(files_changed, fn, val) {
+		if(strstr(fn, ".fmt.jdiff")) {
+			json_t *patch = stack_json_resolve(fn, nullptr);
+			loopmod_fmt(patch, fn);
+			json_decref(patch);
+		}
+	}
+}
+/// =====
