fca55bb0a714ae2fcee728bb79a17a64fe474bd6
Egor <egor@opensrc.club>
2018-02-25 19:44:17 +0200

    Add manifest for thcrap_configure

--- thcrap_configure/src/winconsole.cpp
+++ thcrap_configure/src/winconsole.cpp
@@ -12,12 +12,6 @@
 #include <memory>
 #include <thread>
 
-// TODO: preferrably do a separate manifest file
-#pragma comment(lib,"Comctl32.lib")
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 template<typename T>
 using EventPtr = const std::shared_ptr<std::promise<T>>*;
 
