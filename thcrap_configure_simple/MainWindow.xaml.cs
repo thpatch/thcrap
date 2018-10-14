ea27797e8bc93841c6500db8ae69b1236072be6e
brliron <brliron@hotmail.fr>
2018-10-14 19:19:40 +0200

    Use the WPF Toolkit for the thcrap_configure wizard

--- thcrap_configure_simple/MainWindow.xaml.cs
+++ thcrap_configure_simple/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,40 @@ namespace thcrap_configure_simple
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Wizard_PageChanged(object sender, RoutedEventArgs e)
+        {
+            if (wizard.CurrentPage == Page1)
+            {
+                new Thread(() =>
+                {
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() => wizard.CurrentPage = Page2);
+                }).Start(); 
+            }
+            else if (wizard.CurrentPage == Page3)
+            {
+                new Thread(() =>
+                {
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() => wizard.CurrentPage = Page4);
+                }).Start();
+            }
+            else if (wizard.CurrentPage == Page5)
+            {
+                new Thread(() =>
+                {
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() =>
+                    {
+                        Page5Text.Text = "Downloading and installing files...";
+                        Page5Filename.Text = @"C:\Fake\Path\th16.exe";
+                    });
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() => wizard.CurrentPage = LastPage);
+                }).Start();
+            }
         }
     }
 }
