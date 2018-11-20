1870f5592093c66f999078ded7d6b4a5269226e1
nmlgc <nmlgc@nmlgc.net>
2018-11-20 18:46:39 +0100

    BGM modding: Add a shared base class for PCM formats.
    Otherwise, we'd be duplicating that code between all of the planned
    formats. And yeah, we're "restricting" codec_t to PCM now, because who
    knows if I ever get to actually do tracker/chiptune formats.
    
    Funded by Splashman.

--- thcrap_bgmmod/src/bgmmod.cpp
+++ thcrap_bgmmod/src/bgmmod.cpp
@@ -15,6 +15,68 @@
 const stringref_t LOOP_INFIX = ".loop";
 /// ----------------
 
+/// Track class
+/// -----------
+void track_pcm_t::decode(void *_buf, size_t size)
+{
+	auto *buf = (uint8_t*)_buf;
+	assert(buf);
+	while(size > 0) {
+		auto ret = cur->part_decode(buf, size);
+		if(ret == (size_t)-1) {
+			// TODO: Error?
+			return;
+		} else if(ret == 0) {
+			if((cur == intro.get()) && (loop != nullptr)) {
+				cur = loop.get();
+			}
+			cur->part_seek_to_sample(0);
+		}
+		buf += ret;
+		size -= ret;
+	}
+}
+
+void track_pcm_t::seek_to_byte(size_t byte)
+{
+	auto *actual_looping_segment = loop ? loop.get() : intro.get();
+	if(byte < intro->part_bytes) {
+		cur = intro.get();
+	} else {
+		cur = actual_looping_segment;
+		auto loop_length = cur->part_bytes;
+		byte -= intro->part_bytes;
+		byte %= loop_length;
+	}
+	cur->part_seek_to_sample(byte / (pcmf.bitdepth / 8) / pcmf.channels);
+}
+
+std::unique_ptr<track_pcm_t> pcm_open(
+	pcm_part_open_t &codec_open, HANDLE &&intro, HANDLE &&loop
+)
+{
+	std::unique_ptr<pcm_part_t> intro_sgm;
+	std::unique_ptr<pcm_part_t> loop_sgm;
+
+	intro_sgm = codec_open(std::move(intro));
+	if(!intro_sgm) {
+		if(loop != INVALID_HANDLE_VALUE) {
+			CloseHandle(loop);
+		}
+		return nullptr;
+	}
+	if(loop != INVALID_HANDLE_VALUE) {
+		loop_sgm = codec_open(std::move(loop));
+		if(!loop_sgm) {
+			return nullptr;
+		}
+	}
+	return std::make_unique<track_pcm_t>(
+		std::move(intro_sgm), std::move(loop_sgm)
+	);
+}
+/// -----------
+
 /// Error reporting and debugging
 /// -----------------------------
 void bgmmod_verrorf(const char *text, va_list va)
--- thcrap_bgmmod/src/bgmmod.hpp
+++ thcrap_bgmmod/src/bgmmod.hpp
@@ -46,13 +46,68 @@ struct track_t {
 
 	virtual ~track_t() {}
 };
+
+// Base class for an individual intro or loop file.
+// Should be derived for each supported codec.
+struct pcm_part_t {
+	const pcm_format_t pcmf;
+	// Size of this part, in decoded PCM bytes according to [pcmf].
+	const size_t part_bytes;
+
+	// Single decoding call. Should return the number of bytes actually
+	// decoded, which can be less than [size]. If an error occured, it
+	// should return -1, and show a message box.
+	virtual size_t part_decode(void *buf, size_t size) = 0;
+	// Seeks to the raw decoded, non-interleaved audio sample (byte
+	// divided by bitdepth and number of channels). Guaranteed to be
+	// less than the total number of samples in the stream.
+	virtual void part_seek_to_sample(size_t sample) = 0;
+
+	pcm_part_t(pcm_format_t pcmf, size_t part_bytes)
+		: pcmf(pcmf), part_bytes(part_bytes) {
+	}
+	virtual ~pcm_part_t() {}
+};
+
+// Generic implementation for PCM-based codecs, with separate intro and
+// loop files.
+struct track_pcm_t : public track_t {
+	std::unique_ptr<pcm_part_t> intro;
+	std::unique_ptr<pcm_part_t> loop;
+	pcm_part_t *cur;
+
+	/// track_t functions
+	void decode(void *buf, size_t size);
+	void seek_to_byte(size_t byte);
+
+	track_pcm_t(
+		std::unique_ptr<pcm_part_t> &&intro_in,
+		std::unique_ptr<pcm_part_t> &&loop_in
+	) : track_t(
+		intro_in->pcmf,
+		intro_in->part_bytes + (loop_in ? loop_in->part_bytes : 0)
+	) {
+		intro = std::move(intro_in);
+		loop = std::move(loop_in);
+		cur = intro.get();
+	}
+	virtual ~track_pcm_t() {}
+};
+
+// Opens [stream] as a part of a modded track, using a specific codec.
+// Should show a message box if [stream] is not a valid file for this codec.
+typedef std::unique_ptr<pcm_part_t> pcm_part_open_t(HANDLE &&stream);
+
+std::unique_ptr<track_pcm_t> pcm_open(
+	pcm_part_open_t &codec_open, HANDLE &&intro, HANDLE &&loop
+);
 /// -----------
 
 /// Codecs
 /// ------
 struct codec_t {
 	stringref_t ext;
-	std::unique_ptr<track_t> (*open)(HANDLE &&intro, HANDLE &&loop);
+	pcm_part_open_t &open;
 };
 /// ------
 
