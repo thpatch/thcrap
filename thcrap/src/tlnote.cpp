26768b01b1eea688deed97834d08458405850b19
nmlgc <nmlgc@nmlgc.net>
2019-01-15 06:15:04 +0100

    thcrap: TL notes: Include the separating character in the split-off string.
    Moves the future distinction between immediate and pre-rendered TL
    notes to the tlnote module itself.
    
    Funded by zorg.

--- thcrap/src/tlnote.cpp
+++ thcrap/src/tlnote.cpp
@@ -15,13 +15,25 @@
 logger_t tlnote_log("TL note error");
 /// -----------------------------
 
-THCRAP_API void tlnote_show(const stringref_t tlnote)
+/// Structures
+/// ----------
+tlnote_t::tlnote_t(const stringref_t str)
+	: str((tlnote_string_t *)str.str), len(str.len - 1) {
+	assert(str.str[0] == TLNOTE_INLINE);
+	assert(str.len > 1);
+}
+/// ----------
+
+THCRAP_API void tlnote_show(const tlnote_t tlnote)
 {
 	if(!tlnote.str) {
 		return;
 	}
+	if(tlnote.str->type != TLNOTE_INLINE) {
+		return;
+	}
 	// TODO: For now...
-	log_printf("(TL note: %.*s)\n", tlnote.len, tlnote.str);
+	log_printf("(TL note: %.*s)\n", tlnote.len, tlnote.str->note);
 }
 
 THCRAP_API tlnote_split_t tlnote_find(stringref_t text)
@@ -43,8 +55,9 @@ THCRAP_API tlnote_split_t tlnote_find(stringref_t text)
 		p++;
 	}
 	if(sepchar_ptr) {
-		auto tlnote = (sepchar_ptr + 1);
-		return { sepchar_ptr - text.str, tlnote, p - tlnote };
+		tlnote_t tlnote = { { sepchar_ptr, p - sepchar_ptr } };
+		return { sepchar_ptr - text.str, tlnote };
 	}
 	return { text.len };
 }
+
--- thcrap/src/tlnote.hpp
+++ thcrap/src/tlnote.hpp
@@ -13,6 +13,26 @@
 // regular text (before).
 #define TLNOTE_INLINE 0x14
 
+typedef struct {
+	char type;
+	char note[1];
+} tlnote_string_t;
+
+struct tlnote_t {
+	const tlnote_string_t *str;
+	// *Not* including the type character.
+	int len;
+
+	operator bool() const { return str != nullptr; }
+
+	tlnote_t()
+		: str(nullptr), len(0) {
+	}
+	// Not exported; non-empty instances are only supposed to be
+	// created by tlnote_find().
+	tlnote_t(const stringref_t str);
+};
+
 // Information for splitting a line of text into the regular part and its TL
 // note, if there is one.
 struct tlnote_split_t {
@@ -20,14 +40,13 @@ struct tlnote_split_t {
 	// Equal to the length of the input string if there is none.
 	int regular_len;
 
-	// TL note making up the rest of the line, or a nullptr if there is none.
-	// (Not a stringref_t because of that!)
-	const char *tlnote;
-	int tlnote_len;
+	// TL note making up the rest of the line.
+	// Evaluates to false if there is none.
+	const tlnote_t tlnote;
 };
 
 // Directly shows [tlnote].
-THCRAP_API void tlnote_show(const stringref_t tlnote);
+THCRAP_API void tlnote_show(const tlnote_t tlnote);
 
 // Searches [text] for a TL note.
 THCRAP_API tlnote_split_t tlnote_find(stringref_t text);
