thpatch/thcrap

commit b67116db44f006ae1763ae9e1c7a172f4e3c2457

Move patches to patches/

thcrap/src/patchfile.cpp
@@ -177,6 +177,7 @@ char* fn_for_patch(const json_t *patch_info, const char *fn)
 	patch_fn = (char*)malloc(patch_fn_len * sizeof(char));
 
 	strncpy(patch_fn, json_string_value(archive), archive_len);
+	PathAddBackslashU(patch_fn);
 	strcat(patch_fn, fn);
 	str_slash_normalize(patch_fn);
 	return patch_fn;
@@ -286,7 +287,7 @@ json_t* patch_build(const json_t *sel)
 	const char *repo_id = json_array_get_string(sel, 0);
 	const char *patch_id = json_array_get_string(sel, 1);
 	return json_pack("{ss+++}",
-		"archive", repo_id, "/", patch_id, "/"
+		"archive", "patches/", repo_id, "/", patch_id, "/"
 	);
 }
 

thcrap_configure/src/configure.cpp
@@ -220,7 +220,8 @@ int __cdecl win32_utf8_main(int argc, const char *argv[])
 	);
 	pause();
 
-
+	CreateDirectoryU("patches", NULL);
+	SetCurrentDirectoryU("patches");
 	if (RepoDiscoverAtURL_wrapper(start_repo, id_cache, url_cache, file_write_error)) {
 		goto end;
 	}
@@ -233,6 +234,7 @@ int __cdecl win32_utf8_main(int argc, const char *argv[])
 		pause();
 		goto end;
 	}
+	SetCurrentDirectoryU("..");
 	sel_stack = SelectPatchStack(repo_list);
 	if (json_array_size(sel_stack)) {
 		json_t *new_cfg_patches = json_object_get(new_cfg, "patches");

commit 29f16632f6bedd85e849034b01c3a8e8d4b7d83d

patch: fix the json_pack format specifier in patch_build
Fixes a bug introduced in b67116db44f006ae1763ae9e1c7a172f4e3c2457 where the final '/' isn't written in the patch stack. This caused a bug in the logs, and could break some other place assuming a / here.

thcrap/src/patchfile.cpp
@@ -286,7 +286,7 @@ json_t* patch_build(const json_t *sel)
 {
 	const char *repo_id = json_array_get_string(sel, 0);
 	const char *patch_id = json_array_get_string(sel, 1);
-	return json_pack("{ss+++}",
+	return json_pack("{ss++++}",
 		"archive", "repos/", repo_id, "/", patch_id, "/"
 	);
 }