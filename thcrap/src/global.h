bc62180bc9f179b9c756777ab4e7126f66579b2e
doremyr3d <francesco@calcagnini.it>
2019-11-06 14:51:20 +0100

    thcrap: thcrap: caller should define default_value when using globalconfig_get
    Also, globalconfig_get_* assign ENOENT to errno instead of 1 when key isn't in globalconfig

--- thcrap/src/global.h
+++ thcrap/src/global.h
@@ -30,14 +30,14 @@ void runconfig_set(json_t *new_run_cfg);
 const json_t *runconfig_title_get(void);
 
 // Returns the value matching key in config converted in bool
-// If key isn't in config it returns false with errno 1
-BOOL globalconfig_get_boolean(char* key);
+// If key isn't in config it returns default_value with errno ENOENT
+BOOL globalconfig_get_boolean(char* key, const BOOL default_value);
 // Sets the value in config and then writes the result on disk
 // It returns what json_dump_file returns
 int globalconfig_set_boolean(char* key, const BOOL value);
 // Returns the value matching key in config converted in long long
-// If key isn't in config it returns false with errno 1
-long long globalconfig_get_integer(char* key);
+// If key isn't in config it returns default_value with errno ENOENT
+long long globalconfig_get_integer(char* key, const long long default_value);
 // Sets the value in config and then writes the result on disk
 // It returns what json_dump_file returns
 int globalconfig_set_integer(char* key, const long long value);
