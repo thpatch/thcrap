d816648235945be8fcea4b7f84daf985b0dc6f4b
brliron <brliron@hotmail.fr>
2020-02-21 10:08:06 +0100

    Create an empty thcrap_configure_wxwidgets project

---
+++ thcrap_configure_wxwidgets/src/wxconfigure.cpp
@@ -0,0 +1,16 @@
+/**
+  * Touhou Community Reliant Automatic Patcher
+  * Cheap command-line patch stack configuration tool
+  */
+
+#include <thcrap.h>
+#include "configure.h"
+
+#include <win32_utf8/entry_winmain.c>
+#include "thcrap_i18n/src/thcrap_i18n.h"
+int __cdecl win32_utf8_main(int argc, const char *argv[])
+{
+	i18n_lang_init(THCRAP_I18N_APPDOMAIN);
+    log_mbox(nullptr, MB_OK, "Hello world!");
+	return 0;
+}
---
+++ thcrap_configure_wxwidgets/src/wxconfigure.h
@@ -0,0 +1,6 @@
+/**
+  * Touhou Community Reliant Automatic Patcher
+  * wxWidgets-based patch stack configuration tool
+  */
+
+#pragma once
