a7880244dda86231482286117d16e56ecf7f010a
Tudi20 <tudorkae@gmail.com>
2020-04-23 01:00:42 +0200

    Fix include typo

--- thcrap_configure_wxwidgets/src/wxconfigure.cpp
+++ thcrap_configure_wxwidgets/src/wxconfigure.cpp
@@ -4,7 +4,7 @@
   */
 
 #include <thcrap.h>
-#include "configure.h"
+#include "wxconfigure.h"
 
 #include <win32_utf8/entry_winmain.c>
 #include "thcrap_i18n/src/thcrap_i18n.h"
@@ -12,5 +12,6 @@ int __cdecl win32_utf8_main(int argc, const char *argv[])
 {
 	i18n_lang_init(THCRAP_I18N_APPDOMAIN);
     log_mbox(nullptr, MB_OK, "Hello world!");
+	
 	return 0;
 }
