66c1b42c126d13f1c4ffffd22e0304a49ea6054e
brliron <brliron@hotmail.fr>
2020-09-07 11:07:15 +0200

    netcode: Update http_wininet to the new API
    I didn't try to compile, so it probably doesn't work.

--- netcode/http_wininet.cpp
+++ netcode/http_wininet.cpp
@@ -3,11 +3,8 @@
 #include <vector>
 #include "http_wininet.h"
 
-double perffreq;
-
 HttpHandle::HttpHandle()
 {
-    LARGE_INTEGER pf;
     DWORD ignore = 1;
     // DWORD timeout = 500;
 
@@ -16,9 +13,6 @@ HttpHandle::HttpHandle()
     auto self_version = L"0.0.1";
     auto os = L"Windows 10 (hardcoded string)";
     std::wstring agent = std::wstring(self_name) + L"/" + self_version + L" (" + os + L")";
-
-    QueryPerformanceFrequency(&pf);
-    perffreq = (double)pf.QuadPart;
 
     this->internet = InternetOpenW(agent.c_str(), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
     if (this->internet == nullptr) {
@@ -50,13 +44,38 @@ HttpHandle::~HttpHandle()
     }
 }
 
-bool HttpHandle::download(const std::string& url, std::function<size_t(const uint8_t*, size_t)> writeCallback, std::function<bool(size_t, size_t)> progressCallback)
+class ScopedHInternet
+{
+private:
+    HINTERNET hInternet;
+
+public:
+    ScopedHInternet(HINTERNET hInternet)
+        : hInternet(hInternet)
+    {}
+    ~ScopedHInternet()
+    {
+        if (this->hInternet) {
+	        InternetCloseHandle(this->hInternet);
+        }
+    }
+    operator HINTERNET()
+    {
+        return this->hInternet;
+    }
+
+    ScopedHInternet(const HInternet& src) = delete;
+    ScopedHInternet(HInternet&& src) = delete;
+    const ScopedHInternet& operator=(const HInternet& src) = delete;
+    const ScopedHInternet& operator=(HInternet&& src) = delete;
+}
+
+Status HttpHandle::download(const std::string& url, std::function<size_t(const uint8_t*, size_t)> writeCallback, std::function<bool(size_t, size_t)> progressCallback)
 {
 	DWORD byte_ret = sizeof(DWORD);
 	DWORD http_stat = 0;
-	HINTERNET hFile = NULL;
 
-	hFile = InternetOpenUrlA(
+	ScopedHInternet hFile = InternetOpenUrlA(
 		this->internet, url.c_str(), NULL, 0,
 		INTERNET_FLAG_RELOAD | INTERNET_FLAG_KEEP_CONNECTION, 0
 	);
@@ -68,30 +87,34 @@ bool HttpHandle::download(const std::string& url, std::function<size_t(const uin
 		DWORD inet_ret = GetLastError();
 		switch (inet_ret) {
 		case ERROR_INTERNET_NAME_NOT_RESOLVED:
-			log_printf("Could not resolve hostname\n");
-			return false;
+			log_printf("%s: could not resolve hostname\n", url.c_str());
+			return Status::ServerError;
 		case ERROR_INTERNET_CANNOT_CONNECT:
-			log_printf("Connection refused\n");
-			return false;
+			log_printf("%s: connection refused\n", url.c_str());
+			return Status::ServerError;
 		case ERROR_INTERNET_TIMEOUT:
-			log_printf("timed out\n");
-			return false;
+			log_printf("%s: timed out\n", url.c_str());
+			return Status::ServerError;
 		case ERROR_INTERNET_UNRECOGNIZED_SCHEME:
-			log_printf("Unknown protocol\n");
-			return false;
+			log_printf("%s: unknown protocol\n", url.c_str());
+			return Status::ServerError;
 		default:
-			log_printf("WinInet error %d\n", inet_ret);
-			return false;
+			log_printf("%s: WinInet error %d\n", url.c_str(), inet_ret);
+			return Status::Error;
 		}
 	}
 
 	HttpQueryInfo(hFile, HTTP_QUERY_FLAG_NUMBER | HTTP_QUERY_STATUS_CODE,
 		&http_stat, &byte_ret, 0
 	);
-	log_printf("%d ", http_stat);
 	if (http_stat != 200) {
-		InternetCloseHandle(hFile);
-		return false;
+		log_printf("%s: HTTP error code %d\n", url.c_str(), http_stat);
+        if (300 <= http_stat && http_stat <= 499) {
+            return Status::ClientError;
+        }
+        else {
+            return Status::ServerError;
+        }
 	}
 
 	DWORD file_size;
@@ -100,8 +123,7 @@ bool HttpHandle::download(const std::string& url, std::function<size_t(const uin
 	);
 	std::vector<uint8_t> buffer;
 	if (!progressCallback(0, file_size)) {
-		InternetCloseHandle(hFile);
-		return false;
+		return Status::Cancelled;
 	}
 	auto rem_size = file_size;
 
@@ -111,25 +133,23 @@ bool HttpHandle::download(const std::string& url, std::function<size_t(const uin
 			read_size = rem_size;
 		}
 		if (read_size == 0) {
-			log_printf("disconnected\n");
-			InternetCloseHandle(hFile);
-			return false;
+			log_printf("%s: disconnected\n", url.c_str());
+			return Status::ServerError;
 		}
 		buffer.resize(read_size);
-		if (InternetReadFile(hFile, buffer.data(), read_size, &byte_ret)) {
-			rem_size -= byte_ret;
-			if (progressCallback(file_size - rem_size, file_size) == false || writeCallback(buffer.data(), read_size) != read_size) {
-				InternetCloseHandle(hFile);
-				return false;
-			}
+		if (InternetReadFile(hFile, buffer.data(), read_size, &byte_ret) == FALSE) {
+			log_printf("%s: reading error #%d\n", url.c_str(), GetLastError());
+			return return Status::SystemError;
 		}
-		else {
-			log_printf("\nReading error #%d! ", GetLastError());
-			InternetCloseHandle(hFile);
-			return false;
+		rem_size -= byte_ret;
+		if (progressCallback(file_size - rem_size, file_size) == false) {
+            return Status::Cancelled;
+        }
+        if (writeCallback(buffer.data(), read_size) != read_size) {
+		    log_printf("%s: writing error #%d\n", url.c_str(), GetLastError());
+			return Status::SystemError;
 		}
 	}
 
-	InternetCloseHandle(hFile);
-	return true;
+	return Status::Ok;
 }
--- netcode/http_wininet.h
+++ netcode/http_wininet.h
@@ -7,6 +7,23 @@
 
 class HttpHandle
 {
+public:
+    enum class Status
+    {
+        // 200 - success
+        Ok,
+        // 3XX and 4XX - file not found, not accessible, moved, etc.
+        ClientError,
+        // 5XX errors - server errors, further requests are likely to fail.
+        // Also covers weird error codes like 1XX and 2XX which we shouldn't see.
+        ServerError,
+        // Download cancelled by the progress callback, or another client
+        // declared the server as dead
+        Cancelled,
+        // Error returned by the download library or by the write callback
+        Error
+    };
+
 private:
     HINTERNET internet;
 
@@ -17,7 +34,7 @@ public:
     HttpHandle& operator=(HttpHandle& other) = delete;
     ~HttpHandle();
 
-    bool download(const std::string& url, std::function<size_t(const uint8_t*, size_t)> writeCallback, std::function<bool(size_t, size_t)> progressCallback);
+    Status download(const std::string& url, std::function<size_t(const uint8_t*, size_t)> writeCallback, std::function<bool(size_t, size_t)> progressCallback);
 };
 
 
