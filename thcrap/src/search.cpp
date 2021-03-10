983d4dccb9a0aa6ea89f90ac91cb8d33c379f5a5
Egor <egor@opensrc.club>
2021-03-10 06:44:08 +0200

    search: use std::fs for searching
    Also use WaitForMultipleObjects to wait for threads to finish.
    
    Now this is doing an actual mulithreaded search for mulitple disks,
    because it doesn't block immediately after creating the thread.

--- thcrap/src/search.cpp
+++ thcrap/src/search.cpp
@@ -9,40 +9,35 @@
 
 #include <thcrap.h>
 #include <filesystem>
+namespace fs = std::filesystem;
 
 typedef struct alignas(16) {
-	DWORD size_min;
-	DWORD size_max;
+	size_t size_min;
+	size_t size_max;
 	json_t *versions;
 	json_t *found;
 	json_t *result;
-	volatile DWORD cur_threads;
 	CRITICAL_SECTION cs_result;
 } search_state_t;
 
 static search_state_t state;
 
-int SearchCheckExe(wchar_t *local_dir, WIN32_FIND_DATAW *w32fd)
+static int SearchCheckExe(const fs::directory_entry &ent)
 {
 	int ret = 0;
-	json_t *ver = identify_by_size(w32fd->nFileSizeLow, state.versions);
+	json_t *ver = identify_by_size((size_t)ent.file_size(), state.versions);
 	if(ver) {
-		WCSLEN_DEC(local_dir);
-		size_t exe_fn_len = local_dir_len + wcslen(w32fd->cFileName) + 2;
-		VLA(wchar_t, exe_fn, exe_fn_len);
 		const char *key;
 		json_t *game_val;
 
-		wcsncpy(exe_fn, local_dir, local_dir_len);
-		exe_fn[local_dir_len - 1] = 0;
-		wcscat(exe_fn, w32fd->cFileName);
-
-		size_t exe_fn_a_len = wcslen(exe_fn)*UTF8_MUL + 1;
+		const std::wstring &exe_fn = ent.path().native();
+		size_t exe_fn_a_len = exe_fn.length()*UTF8_MUL + 1;
 		VLA(char, exe_fn_a, exe_fn_a_len);
-		StringToUTF8(exe_fn_a, exe_fn, exe_fn_a_len);
+		StringToUTF8(exe_fn_a, exe_fn.c_str(), exe_fn_a_len);
 		str_slash_normalize(exe_fn_a);
 
-		ver = identify_by_hash(exe_fn_a, (size_t*)&w32fd->nFileSizeLow, state.versions);
+		size_t file_size = (size_t)ent.file_size();
+		ver = identify_by_hash(exe_fn_a, &file_size, state.versions);
 		if(!ver) {
 			return ret;
 		}
@@ -56,9 +51,9 @@ int SearchCheckExe(wchar_t *local_dir, WIN32_FIND_DATAW *w32fd)
 
 		// Alright, found a game!
 		// Check if it has a vpatch
-		auto vpatch_fn = std::filesystem::path(local_dir) / L"vpatch.exe";
+		auto vpatch_fn = ent.path().parent_path() / L"vpatch.exe";
 		bool use_vpatch = false;
-		if (strstr(key, "_custom") == nullptr && std::filesystem::is_regular_file(vpatch_fn)) {
+		if (strstr(key, "_custom") == nullptr && fs::is_regular_file(vpatch_fn)) {
 			use_vpatch = true;
 		}
 
@@ -88,77 +83,37 @@ int SearchCheckExe(wchar_t *local_dir, WIN32_FIND_DATAW *w32fd)
 		}
 		LeaveCriticalSection(&state.cs_result);
 		VLA_FREE(exe_fn_a);
-		VLA_FREE(exe_fn);
 		ret = 1;
 	}
 	return ret;
 }
 
 
-DWORD WINAPI SearchThread(void *param)
+static DWORD WINAPI SearchThread(void *param)
 {
-	HANDLE hFind;
-	WIN32_FIND_DATAW w32fd;
-	wchar_t *param_dir = (wchar_t*)param;
-
-	// Add the asterisk
-	size_t dir_len = wcslen(param_dir) + 2 + 1;
-	VLA(wchar_t, local_dir, dir_len);
-
-	BOOL ret = 1;
-
-	wcscpy(local_dir, param_dir);
-	wcscat(local_dir, L"*");
-
-	InterlockedIncrement(&state.cur_threads);
-
-	SAFE_FREE(param_dir);
-	hFind = FindFirstFileW(local_dir, &w32fd);
-	while( (hFind != INVALID_HANDLE_VALUE) && ret ) {
-		if(
-			!wcscmp(w32fd.cFileName, L".") ||
-			!wcscmp(w32fd.cFileName, L"..")
-		) {
-			ret = FindNextFileW(hFind, &w32fd);
-			continue;
+	const wchar_t *dir = (const wchar_t *)param;
+	try {
+		for (auto &ent : fs::recursive_directory_iterator(dir, fs::directory_options::skip_permission_denied)) {
+			try {
+				if (ent.is_regular_file()
+					&& PathMatchSpecW(ent.path().c_str(), L"*.exe")
+					&& ent.file_size() >= state.size_min
+					&& ent.file_size() <= state.size_max)
+				{
+					SearchCheckExe(ent);
+				}
+			}
+			catch (fs::filesystem_error &) {}
 		}
-		local_dir[dir_len - 3] = 0;
+	} catch (fs::filesystem_error &) {}
 
-		if(w32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			size_t cur_file_len = dir_len + wcslen(w32fd.cFileName) + 2;
-			wchar_t *cur_file = (wchar_t*)malloc(cur_file_len * sizeof(wchar_t));
-
-			wcsncpy(cur_file, local_dir, dir_len);
-			wcscat(cur_file, w32fd.cFileName);
-			PathAddBackslashW(cur_file);
-
-			SearchThread(cur_file);
-		} else if(
-			(PathMatchSpecW(w32fd.cFileName, L"*.exe")) &&
-			(w32fd.nFileSizeLow >= state.size_min) &&
-			(w32fd.nFileSizeLow <= state.size_max)
-		) {
-			SearchCheckExe(local_dir, &w32fd);
-		}
-		ret = FindNextFileW(hFind, &w32fd);
-	}
-	FindClose(hFind);
-	InterlockedDecrement(&state.cur_threads);
-	VLA_FREE(local_dir);
 	return 0;
 }
 
-void LaunchSearchThread(const char *dir)
+static HANDLE LaunchSearchThread(const wchar_t *dir)
 {
 	DWORD thread_id;
-	HANDLE initial_thread;
-
-	// Freed inside the thread
-	size_t cur_dir_len = strlen(dir) + 1;
-	wchar_t *cur_dir = (wchar_t*)malloc(cur_dir_len * sizeof(wchar_t));
-	StringToUTF16(cur_dir, dir, cur_dir_len);
-	initial_thread = CreateThread(NULL, 0, SearchThread, cur_dir, 0, &thread_id);
-	WaitForSingleObject(initial_thread, INFINITE);
+	return CreateThread(NULL, 0, SearchThread, (void*)dir, 0, &thread_id);
 }
 
 json_t* SearchForGames(const char *dir, json_t *games_in)
@@ -181,9 +136,8 @@ json_t* SearchForGames(const char *dir, json_t *games_in)
 	sizes = json_object_get(state.versions, "sizes");
 	// Error...
 
-	state.size_min = 0xFFFFFFFF;
+	state.size_min = -1;
 	state.size_max = 0;
-	state.cur_threads = 0;
 	state.found = json_object();
 	state.result = games_in ? games_in : json_object();
 	InitializeCriticalSection(&state.cs_result);
@@ -192,32 +146,45 @@ json_t* SearchForGames(const char *dir, json_t *games_in)
 	json_object_foreach(sizes, key, val) {
 		size_t cur_size = atoi(key);
 
-		state.size_min = MIN(cur_size, state.size_min);
-		state.size_max = MAX(cur_size, state.size_max);
+		if (cur_size < state.size_min)
+			state.size_min = cur_size;
+		if (cur_size > state.size_max)
+			state.size_max = cur_size;
 	}
 
-	if(dir && dir[0]) {
-		LaunchSearchThread(dir);
-	} else {
-		char drive_strings[512];
-		char *p = drive_strings;
+	constexpr size_t max_threads = 32;
+	HANDLE threads[max_threads];
+	DWORD count = 0;
 
-		GetLogicalDriveStringsA(512, drive_strings);
-		while(p && p[0]) {
-			UINT drive_type = GetDriveTypeA(p);
+	wchar_t *dir_w = NULL;
+	if(dir && dir[0]) {
+		size_t dir_len = strlen(dir) + 1;
+		dir_w = (wchar_t*)malloc(dir_len * sizeof(wchar_t));
+		StringToUTF16(dir_w, dir, dir_len);
+		if ((threads[count] = LaunchSearchThread(dir_w)) != NULL)
+			count++;
+	} else {
+		wchar_t drive_strings[512];
+		wchar_t *p = drive_strings;
+
+		GetLogicalDriveStringsW(512, drive_strings);
+		while(p && p[0] && count < max_threads) {
+			UINT drive_type = GetDriveTypeW(p);
 			if(
 				(drive_type != DRIVE_CDROM) &&
-				(p[0] != 'A') &&
-				(p[0] != 'a')
+				(p[0] != L'A') &&
+				(p[0] != L'a')
 			) {
-				LaunchSearchThread(p);
+				if ((threads[count] = LaunchSearchThread(p)) != NULL)
+					count++;
 			}
-			p += strlen(p) + 1;
+			p += wcslen(p) + 1;
 		}
 	}
-	while(state.cur_threads) {
-		Sleep(100);
-	}
+	WaitForMultipleObjects(count, threads, TRUE, INFINITE);
+	while (count)
+		CloseHandle(threads[--count]);
+	free(dir_w);
 
 	DeleteCriticalSection(&state.cs_result);
 	json_decref(state.versions);
