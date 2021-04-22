41fd327695042cf3b7b2560e7ff11c1c884e2bfb
brliron <brliron@hotmail.fr>
2021-04-22 20:38:15 +0200

    thcrap_tasofro: improve nut instructions patching
    Add replace_instruction, apply the insert_instructions in the correct
    order, and probably 1 or 2 other things I forgot.

--- thcrap_tasofro/src/act-nut.cpp
+++ thcrap_tasofro/src/act-nut.cpp
@@ -8,54 +8,108 @@
   */
 
 #include <thcrap.h>
+#include <algorithm>
 #include <libs/135tk/Act-Nut-lib/exports.hpp>
 #include "thcrap_tasofro.h"
 #include "act-nut.h"
 
-static void patch_actnut_as_object(ActNut::Object* elem, json_t *json)
+void json_object_numkeys_foreach(json_t *obj, std::function<void (int key, json_t *value)> function)
 {
-	size_t offset_for_instructions = 0;
+	std::vector<int> keys;
 
-	json_t *insert_instructions = json_object_get(json, "insert_instructions");
-	if (insert_instructions) {
-		Nut::SQFunctionProto *func = dynamic_cast<Nut::SQFunctionProto*>(elem);
-		if (func) {
-			const char *instruction_number_s;
-			json_t *instruction_flexarray;
-			json_object_foreach(insert_instructions, instruction_number_s, instruction_flexarray) {
-				size_t instruction_number = atoi(instruction_number_s);
-				size_t i;
-				json_t *instruction;
-				json_flex_array_foreach(instruction_flexarray, i, instruction) {
-					func->insertInstruction(instruction_number + offset_for_instructions, json_string_value(instruction));
-					offset_for_instructions++;
-				}
-			}
-		}
-		else {
-			log_print("Act/Nut: insert_instructions found for a non-function element\n");
+	for (void *iter = json_object_iter(obj); iter != nullptr; iter = json_object_iter_next(obj, iter)) {
+		const char *key = json_object_iter_key(iter);
+		keys.push_back(atoi(key));
+	}
+
+	std::sort(keys.begin(), keys.end());
+	for (int key : keys) {
+		std::string key_s = std::to_string(key);
+		function(key, json_object_get(obj, key_s.c_str()));
+	}
+}
+
+static void patch_SQFunctionProto(Nut::SQFunctionProto *func, json_t *add_literals, json_t *replace_instructions, json_t *insert_instructions, json_t *remove_instructions)
+{
+	// Add literals to the function. They can be used by instructions in insert_instructions and set_instructions.
+	if (add_literals) {
+		size_t i;
+		json_t *value;
+		json_flex_array_foreach(add_literals, i, value) {
+			func->addLiteral(json_string_value(value));
 		}
 	}
 
-	json_t *remove_instructions = json_object_get(json, "remove_instructions");
+	// Remove instructions. They aren't removed (only replaced by nop instructions), so you don't need to change any offset.
 	if (remove_instructions) {
+		Nut::vector *instructions = dynamic_cast<Nut::vector*>((*func)["Instructions"]);
+
+		size_t i;
+		json_t *value;
+		json_flex_array_foreach(remove_instructions, i, value) {
+			size_t line = (size_t)json_integer_value(value);
+			if (line >= instructions->size()) {
+				log_printf("Act/Nut: insert_instructions: instruction number too big (%u)\n", line);
+				continue;
+			}
+
+			Nut::SQInstruction *instruction = dynamic_cast<Nut::SQInstruction*>((*instructions)[line]);
+			// line instructions are used only when a Squirrel debugger is present. We use them as nop.
+			*(*instruction)["0"] = "line";
+			*(*instruction)["2"] = "0";
+		}
+	}
+
+	// Replace instructions. Basically the same as setting "/Instructions/X" from the root object,
+	// but this one is applied when it makes the most sense: after literals insertion (so they are
+	// available), but before instructions insertion (so the instruction offsets don't change).
+	if (replace_instructions) {
+		Nut::vector *instructions = dynamic_cast<Nut::vector*>((*func)["Instructions"]);
+
+		const char *key;
+		json_t *value;
+		json_object_foreach(replace_instructions, key, value) {
+			ActNut::Object *instruction = (*instructions)[key];
+			if (!instruction) {
+				log_printf("Act/Nut: set_instructions: instruction not found (%s)\n", key);
+				continue;
+			}
+
+			*instruction = json_string_value(value);
+		}
+	}
+
+	// Insert new instructions.
+	// The offsets are always relative to the original file.
+	// Note that you will need to fix jump targets if you insert instructions between a jump and its target.
+	if (insert_instructions) {
+		size_t offset_for_instructions = 0;
+
+		json_object_numkeys_foreach(insert_instructions, [func, &offset_for_instructions](int instruction_number, json_t *instruction_flexarray) {
+			size_t i;
+			json_t *instruction;
+			json_flex_array_foreach(instruction_flexarray, i, instruction) {
+				func->insertInstruction(instruction_number + offset_for_instructions, json_string_value(instruction));
+				offset_for_instructions++;
+			}
+		});
+	}
+}
+
+static void patch_actnut_as_object(ActNut::Object* elem, json_t *json)
+{
+	json_t *add_literals = json_object_get(json, "add_literals");
+	json_t *replace_instructions = json_object_get(json, "replace_instructions");
+	json_t *insert_instructions = json_object_get(json, "insert_instructions");
+	json_t *remove_instructions = json_object_get(json, "remove_instructions");
+
+	if (add_literals || replace_instructions || insert_instructions || remove_instructions) {
 		Nut::SQFunctionProto *func = dynamic_cast<Nut::SQFunctionProto*>(elem);
 		if (func) {
-			Nut::vector *instructions = dynamic_cast<Nut::vector*>((*func)["instructions"]);
-
-			size_t i;
-			json_t *value;
-			json_flex_array_foreach(remove_instructions, i, value) {
-				size_t line = (size_t)json_integer_value(value);
-				if (line + offset_for_instructions >= instructions->size()) {
-					log_printf("Act/Nut: insert_instructions: instruction number too big (%u)\n", line);
-					continue;
-				}
-
-				Nut::SQInstruction *instruction = dynamic_cast<Nut::SQInstruction*>((*instructions)[line + offset_for_instructions]);
-				// line instructions are used only when a Squirrel debugger is present. We use them as nop.
-				*(*instruction)[0] = "line";
-			}
+			patch_SQFunctionProto(func, add_literals, replace_instructions, insert_instructions, remove_instructions);
+		}
+		else {
+			log_print("Act/Nut: function modificator found for a non-function element\n");
 		}
 	}
 }
