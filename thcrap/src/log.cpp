d7fce40ff38f849a50ba9d018675b5d228ad5b60
32th System <intensonat@gmail.com>
2021-07-14 13:32:39 +0200

    thcrap: remove remaints of something unused

--- thcrap/src/log.cpp
+++ thcrap/src/log.cpp
@@ -12,20 +12,6 @@
 #include <fcntl.h>
 #include <ThreadPool.h>
 
-#include <powrprof.h>
-// See remarks of: https://docs.microsoft.com/en-us/windows/win32/power/processor-power-information-str
-typedef struct _PROCESSOR_POWER_INFORMATION {
-	ULONG Number;
-	ULONG MaxMhz;
-	ULONG CurrentMhz;
-	ULONG MhzLimit;
-	ULONG MaxIdleState;
-	ULONG CurrentIdleState;
-} PROCESSOR_POWER_INFORMATION, * PPROCESSOR_POWER_INFORMATION;
-
-
-
-
 // -------
 // Globals
 // -------
