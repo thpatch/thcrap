7c7dd2cd13d4651188305286cc2fbbcf3842726c
32th-System <intensonat@gmail.com>
2021-11-03 12:43:52 +0100

    Tasofro: update th175 crypt code

--- thcrap_tasofro/src/th175.cpp
+++ thcrap_tasofro/src/th175.cpp
@@ -78,139 +78,48 @@ extern "C" int BP_th175_open_file(x86_reg_t *regs, json_t *bp_info)
 	return 1;
 }
 
-#define IMUL_1(x) imul(&edx, &eax, eax, x);
-#define IMUL_3(x, y, z) imul(NULL, &x, y, z)
-void imul(uint32_t* dst_high, uint32_t* dst_low, uint32_t src_1, uint32_t src_2)
+void do_partial_xor(uint8_t* dst, uint8_t* src, size_t size)
 {
-	int64_t ret = ((int64_t)(int32_t)src_1) * (int32_t)src_2;
-	if (dst_high) {
-		*dst_high = ret >> 32;
+	for (size_t i = 0; i < size; i++) {
+		dst[i] ^= src[i];
 	}
-	*dst_low = (uint32_t)ret;
+}
+
+// More readable version of the decrypt code.
+// This one also doesn't require the input buffer size to be divisible by 4
+uint32_t do_decrypt_step(uint32_t key)
+{
+	int64_t a = key * 0x5E4789C9ll;
+	uint32_t b = (a >> 0x2E) + (a >> 0x3F);
+	uint32_t ret = (key - b * 0xADC8) * 0xBC8F + b * 0xFFFFF2B9;
+	if ((int32_t)ret <= 0) {
+		ret += 0x7FFFFFFF;
+	}
+	return ret;
 }
 
 void th175_crypt_file(uint8_t* buffer, size_t size, size_t offset_in_file)
 {
-	uint32_t* buffer_int_array = (uint32_t*)buffer;
-	uint32_t eax, ebx, ecx, edx, /* ebp, */ esi, edi;
+	// File key, derived from the file's size and its offset in the archive file.
+	uint32_t file_key = size ^ offset_in_file;
 
-	// uint32_t param1 = (uint32_t)buffer;
-	uint32_t param2 = 0;
-	uint32_t param3 = size;
-	// Param 4 is an object with the header size and the file size
-
-	// eax = param4
-	edx = param3;
-	if (1) {
-		ecx = size;
-		ecx ^= offset_in_file; // from param4
-		edx = param3 >> 2;
-	}
-
-	eax = param2 >> 2;
-	// ebp = param1;
-	ecx += eax;
-
-	while (edx > 0) {
-		uint32_t stack_1 = edx;
-		eax = ecx;
-		edx = 0x5E4789C9;
-		esi = ecx;
-		edi = 0x5E4789C9;
-
-		// copy
-		IMUL_1(edx);
-		eax = edx;
-		edx = (int32_t)edx >> 0x0E;
-		eax >>= 0x1F;
-		edx += eax;
-		IMUL_3(eax, edx, 0xADC8);
-		IMUL_3(edx, edx, 0xFFFFF2B9);
-		esi -= eax;
-		IMUL_3(eax, esi, 0xBC8F);
-		ebx = eax + edx;
-		esi = eax + edx + 0x7FFFFFFF;
-
-		if ((int32_t)ebx > 0) {
-			esi = ebx;
-		}
-		eax = esi;
-		ebx = esi;
-
-		// paste
-		IMUL_1(edi); // edi instead of edx
-		eax = edx;
-		edx = (int32_t)edx >> 0x0E;
-		eax >>= 0x1F;
-		edx += eax;
-		IMUL_3(eax, edx, 0xADC8);
-		IMUL_3(edx, edx, 0xFFFFF2B9);
-		ebx -= eax; // ebx instead of esi
-		IMUL_3(eax, ebx, 0xBC8F); // same
-		edi = eax + edx; // edi instead of ebx
-		ebx = eax + edx + 0x7FFFFFFF; // ebx instead of edi
-
-		edx = 0x5E4789C9;
-		if ((int32_t)edi > 0) {
-			ebx = edi;
+	for (size_t pos = 0; pos < size; pos += 4) {
+		uint32_t xor = 0;
+		uint32_t tmp_key = file_key;
+		for (size_t i = 0; i < 4; i++) {
+			tmp_key = do_decrypt_step(tmp_key);
+			xor = (xor << 8) | (tmp_key & 0xFF);
 		}
 
-		esi <<= 8;
-		eax = ebx;
-		edi = ebx & 0xFF;
-
-		// paste
-		IMUL_1(edx); // edx instead of edi
-		edi |= esi; // Added in the middle
-		eax = edx;
-		edx = (int32_t)edx >> 0x0E;
-		eax >>= 0x1F;
-		edx += eax;
-		IMUL_3(eax, edx, 0xADC8);
-		IMUL_3(edx, edx, 0xFFFFF2B9);
-		ebx -= eax; // ebx instead of esi
-		IMUL_3(eax, ebx, 0xBC8F); // same
-		esi = eax + edx; // esi instead of ebx
-		ebx = eax + edx + 0x7FFFFFFF; // ebx instead of edi
-
-		edx = 0x5E4789C9;
-		if ((int32_t)esi > 0) {
-			ebx = esi;
+		if (pos + 4 < size) {
+			*(uint32_t*)(buffer + pos) ^= xor;
 		}
-
-		edi <<= 8;
-		eax = ebx;
-		esi = ebx & 0xFF;
-
-		// paste
-		IMUL_1(edx); // edx instead of edi
-		esi |= edi; // Added in the middle
-		eax = edx;
-		edx = (int32_t)edx >> 0x0E;
-		eax >>= 0x1F;
-		edx += eax;
-		IMUL_3(eax, edx, 0xADC8);
-		IMUL_3(edx, edx, 0xFFFFF2B9);
-		ebx -= eax; // ebx instead of esi
-		IMUL_3(eax, ebx, 0xBC8F); // same
-		edi = eax + edx; // edi instead of ebx
-		eax = eax + edx + 0xFF; // Completely different
-
-		edx = stack_1;
-		if ((int32_t)edi > 0) {
-			eax = edi;
+		else {
+			do_partial_xor(buffer + pos, (uint8_t*)&xor, size - pos);
 		}
-		esi <<= 8;
-		ecx++;
-		eax &= 0xFF;
-		eax |= esi;
-		*buffer_int_array ^= eax;
-		buffer_int_array++;
-		edx--;
+		file_key++;
 	}
 }
-#undef IMUL_1
-#undef IMUL_3
 
 extern "C" int BP_th175_replaceReadFile(x86_reg_t *regs, json_t *bp_info)
 {
