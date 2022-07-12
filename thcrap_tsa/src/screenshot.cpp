cce9f3d65035e1355c5ec8dd7d91b30cd46ad085
32th-System <intensonat@gmail.com>
2022-07-12 23:59:01 +0200

    TSA: fix png screenshots
    THis is very hacked together at the moment, I just wanted it to work
    somehow

--- thcrap_tsa/src/screenshot.cpp
+++ thcrap_tsa/src/screenshot.cpp
@@ -18,20 +18,20 @@ static HANDLE hScreenshot = NULL;
 static std::vector<uint8_t> bmp;
 
 HANDLE WINAPI screenshot_CreateFileA(
-	LPSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
+	LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
 	LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
 	DWORD dwFlagsAndAttributes, HANDLE hTemplateFile
 )
 {
-	size_t fn_len = strlen(lpFileName);
 	if (PathMatchSpecU(lpFileName, "*.bmp")) {
 		size_t fn_len = strlen(lpFileName);
-		lpFileName[fn_len - 3] = 'p';
-		lpFileName[fn_len - 2] = 'n';
-		lpFileName[fn_len - 1] = 'g';
+		char* fn_real = strdup_size(lpFileName, fn_len);
+		fn_real[fn_len - 3] = 'p';
+		fn_real[fn_len - 2] = 'n';
+		fn_real[fn_len - 1] = 'g';
 		bmp.clear();
 		return hScreenshot = chain_CreateFileA(
-			lpFileName, dwDesiredAccess, dwShareMode,
+			fn_real, dwDesiredAccess, dwShareMode,
 			lpSecurityAttributes, dwCreationDisposition,
 			dwFlagsAndAttributes, hTemplateFile
 		);
@@ -65,35 +65,45 @@ BOOL WINAPI screenshot_CloseHandle(HANDLE handle) {
 	if (handle == hScreenshot) {
 		hScreenshot = NULL;
 
+		if(!bmp.size())
+			return chain_CloseHandle(handle);
+
 		auto bmp_header = (BITMAPFILEHEADER*)bmp.data();
 		uint8_t* data = bmp.data() + bmp_header->bfOffBits;
 
 		size_t width = 0;
 		size_t height = 0;
-		size_t bitdepth = 0;
 
-		auto bmp_info = (BITMAPINFO*)(bmp.data() + sizeof(BITMAPFILEHEADER));
-		if (bmp_info->bmiHeader.biSize == sizeof(BITMAPINFOHEADER)) {
-			auto bi_real = (BITMAPINFOHEADER*)&bmp_info->bmiHeader;
-			width = bi_real->biWidth;
-			height = bi_real->biHeight;
-			bitdepth = bi_real->biBitCount;
-		}
-		else if (bmp_info->bmiHeader.biSize == sizeof(BITMAPCOREHEADER)) {
-			auto bi_real = (BITMAPCOREHEADER*)&bmp_info->bmiHeader;
+		auto bmp_info = (BITMAPINFOHEADER*)(bmp.data() + sizeof(BITMAPFILEHEADER));
+		if (bmp_info->biSize == sizeof(BITMAPCOREHEADER)) {
+			auto bi_real = (BITMAPCOREHEADER*)&bmp_info;
 			width = bi_real->bcWidth;
 			height = bi_real->bcHeight;
-			bitdepth = bi_real->bcBitCount;
+			//bitdepth = bi_real->bcBitCount;
+		} else {
+			width = bmp_info->biWidth;
+			height = bmp_info->biHeight;
+			//bitdepth = bmp_info->biBitCount;
 		}
-		size_t pitch = bitdepth / (float)8 * width;
-
+		size_t pitch = 3 * width;
 
 		png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 		png_infop info_ptr = png_create_info_struct(png_ptr);
-		png_set_IHDR(png_ptr, info_ptr, width, height, bitdepth, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+		png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 		png_bytep* row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-		for (int i = 0; i < height; ++i) {
-			row_pointers[i] = data + i * pitch;
+		for (int i = 0; i < height; i++) {
+			uint8_t* row = data + i * pitch;
+			for (int j = 0; j < width * 3; j += 3) {
+				uint8_t px[3];
+				px[0] = row[j];
+				px[1] = row[j + 1];
+				px[2] = row[j + 2];
+
+				row[j]     = px[2];
+				row[j + 1] = px[1];
+				row[j + 2] = px[0];
+			}
+			row_pointers[height - i - 1] = data + i * pitch;
 		}
 		png_set_write_fn(png_ptr, handle, [](png_structp png_ptr, png_bytep data, png_size_t size) {
 			HANDLE hFile = png_get_io_ptr(png_ptr);
@@ -111,6 +121,7 @@ extern "C" TH_EXPORT void screenshot_mod_detour(void) {
 	detour_chain("kernel32.dll", 1,
 		"CreateFileA", screenshot_CreateFileA, &chain_CreateFileA,
 		"WriteFile", screenshot_WriteFile, &chain_WriteFile,
+		"CloseHandle", screenshot_CloseHandle, &chain_CloseHandle,
 		nullptr
 	);
 }
