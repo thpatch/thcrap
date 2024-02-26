cfde6862820152dc6972b2c29a3fdace145d797e
brliron <brliron@hotmail.fr>
2024-02-26 20:10:29 +0100

    Add mirror setup script to release files list

--- scripts/release_build.sh
+++ scripts/release_build.sh
@@ -15,6 +15,7 @@ FILES_LIST="bin/act_nut_lib.dll \
     bin/Microsoft.WindowsAPICodePack.ShellExtensions.dll \
     bin/Microsoft.Xaml.Behaviors.dll \
     bin/scripts/install_dotnet461.sh \
+    bin/scripts/setup_mirror.bat \
     bin/steam_api.dll \
     bin/System.Buffers.dll \
     bin/System.Memory.dll \
